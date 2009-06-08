d787eb52c0855200e1ddb79dafeff4e98e4da56f
jbordner <none@none>
2009-06-08 11:38:03 -0700

    [svn r2538] Debugging compilation for single-precision (p4)
    --HG--
    branch : trunk

--- src/enzo/Grid.h
+++ src/enzo/Grid.h
@@ -1517,6 +1517,18 @@ int CollapseTestInitializeGrid(int NumberOfSpheres,
   // Flag cells that overlap a subgrid (used for analysis).
   int FlagRefinedCells(grid *Subgrid);
   
+#if defined(p8) || defined(p16)
+  // FLOAT version of above
+  inline int IsInVolume( FLOAT *LeftEdge, FLOAT *RightEdge ){
+    for( int i = 0; i < GridRank; i++ ){
+      if( (GridLeftEdge[i] >= RightEdge[i]) ||
+	  (GridRightEdge[i] <= LeftEdge[i]) ){
+	return FALSE;
+      }
+    }
+     return TRUE;
+  }
+#else
   // Check to see if the grid overlaps a volume.
   // Doesn't care about periodicity. */
   inline int IsInVolume( float *LeftEdge, float *RightEdge ){
@@ -1529,21 +1541,11 @@ int CollapseTestInitializeGrid(int NumberOfSpheres,
      return TRUE;
   }
 
-#if defined(p8) || defined(p16)
-  // FLOAT version of above
-  inline int IsInVolume( FLOAT *LeftEdge, FLOAT *RightEdge ){
-    for( int i = 0; i < GridRank; i++ ){
-      if( (GridLeftEdge[i] >= RightEdge[i]) ||
-	  (GridRightEdge[i] <= LeftEdge[i]) ){
-	return FALSE;
-      }
-    }
-     return TRUE;
-  }
 #endif
 
-  // Check to see if a point is in the grid.
-  inline int PointInGrid( float *point ){
+#if defined(p8) || defined(p16)
+  // Check to see if a FLOAT point is in the grid.
+  inline int PointInGrid( FLOAT *point ){
     for( int i = 0; i < GridRank; i++ ){
       if( ((point[i] >= GridLeftEdge[i]) &&
 	  (point[i] <= GridRightEdge[i])) == FALSE )
@@ -1551,10 +1553,9 @@ int CollapseTestInitializeGrid(int NumberOfSpheres,
     }
     return TRUE;
   }
-
-#if defined(p8) || defined(p16)
-  // Check to see if a FLOAT point is in the grid.
-  inline int PointInGrid( FLOAT *point ){
+#else
+  // Check to see if a point is in the grid.
+  inline int PointInGrid( float *point ){
     for( int i = 0; i < GridRank; i++ ){
       if( ((point[i] >= GridLeftEdge[i]) &&
 	  (point[i] <= GridRightEdge[i])) == FALSE )
