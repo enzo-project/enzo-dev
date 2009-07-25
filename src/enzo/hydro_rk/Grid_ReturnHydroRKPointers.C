f60ff5afebb5fb0ab3e6fb865640239f579fcb7a
John Wise <john.h.wise@nasa.gov>
2009-07-25 10:34:22 -0400

    Forgot to add these files.
    --HG--
    branch : week-of-code

---
+++ src/enzo/hydro_rk/Grid_ReturnHydroRKPointers.C
@@ -0,0 +1,128 @@
+/***********************************************************************
+/
+/  GRID CLASS (RETURNS AN ARRAY OF POINTERS THAT ARE COMPATIBLE WITH 
+/              THE HYDRO_RK SOLVERS)
+/
+/  written by: John Wise
+/  date:       July, 2009
+/  modified1:
+/
+/
+************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+#include "macros_and_parameters.h"
+#include "typedefs.h"
+#include "global_data.h"
+#include "Fluxes.h"
+#include "GridList.h"
+#include "ExternalBoundary.h"
+#include "TopGridData.h"
+#include "Grid.h"
+
+int FindField(int field, int farray[], int numfields);
+
+int grid::ReturnHydroRKPointers(float **Prim, bool ReturnMassFractions)
+{
+
+  if (ProcessorNumber != MyProcessorNumber)
+    return SUCCESS;
+
+  if (NumberOfBaryonFields == 0)
+    return SUCCESS;
+
+  int i, n, dim, size, nfield, n0;
+  int DensNum, GENum, TENum, Vel1Num, Vel2Num, Vel3Num;
+  int B1Num, B2Num, B3Num, PhiNum;
+  int DeNum, HINum, HIINum, HeINum, HeIINum, HeIIINum, HMNum, H2INum, H2IINum,
+      DINum, DIINum, HDINum;
+  int MetalNum, SNColourNum;
+
+  /* Add the physical quantities */
+
+  if (HydroMethod == HD_RK) {
+    this->IdentifyPhysicalQuantities(DensNum, GENum, Vel1Num, Vel2Num, 
+				     Vel3Num, TENum);
+    nfield = n0 = NEQ_HYDRO;
+  }
+
+  else if (HydroMethod == MHD_RK) {
+    this->IdentifyPhysicalQuantities(DensNum, GENum, Vel1Num, Vel2Num, 
+				     Vel3Num, TENum, B1Num, B2Num, B3Num, 
+				     PhiNum);
+    nfield = n0 = NEQ_MHD;
+  }
+  
+  Prim[iden] = BaryonField[DensNum];
+  Prim[ivx] = BaryonField[Vel1Num];
+  if (GridRank > 1)
+    Prim[ivy] = BaryonField[Vel2Num];
+  if (GridRank > 2)
+    Prim[ivz] = BaryonField[Vel3Num];
+  Prim[ietot] = BaryonField[TENum];
+  if (DualEnergyFormalism)
+    Prim[ieint] = BaryonField[GENum];
+
+  if (HydroMethod == MHD_RK) {
+    Prim[iBx] = BaryonField[B1Num];
+    Prim[iBy] = BaryonField[B2Num];
+    Prim[iBz] = BaryonField[B3Num];
+  }
+
+  /* Add the species */
+
+  if (MultiSpecies) {
+
+    this->IdentifySpeciesFields(DeNum, HINum, HIINum, HeINum, HeIINum, HeIIINum, 
+				HMNum, H2INum, H2IINum, DINum, DIINum, HDINum);
+
+    //Prim[nfield++] = BaryonField[DeNum];
+    Prim[nfield++] = BaryonField[HINum];
+    Prim[nfield++] = BaryonField[HIINum];
+    Prim[nfield++] = BaryonField[HeINum];
+    Prim[nfield++] = BaryonField[HeIINum];
+    Prim[nfield++] = BaryonField[HeIIINum];
+
+    if (MultiSpecies > 1) {
+      Prim[nfield++] = BaryonField[HMNum];
+      Prim[nfield++] = BaryonField[H2INum];
+      Prim[nfield++] = BaryonField[H2IINum];
+    }
+
+    if (MultiSpecies > 2) {
+      Prim[nfield++] = BaryonField[DINum];
+      Prim[nfield++] = BaryonField[DIINum];
+      Prim[nfield++] = BaryonField[HDINum];
+    }
+
+  } // ENDIF MultiSpecies
+
+  /* Add the colours (NColor is determined in EvolveLevel_RK2) */  
+
+  if ((MetalNum = FindField(Metallicity, FieldType, NumberOfBaryonFields)) 
+      != -1) {
+    Prim[nfield++] = BaryonField[MetalNum];
+    if (MultiMetals || TestProblemData.MultiMetals) {
+      Prim[nfield++] = BaryonField[MetalNum+1];
+      Prim[nfield++] = BaryonField[MetalNum+2];
+    }
+  }
+
+  if ((SNColourNum = FindField(SNColour, FieldType, NumberOfBaryonFields)) 
+      != -1)
+    Prim[nfield++] = BaryonField[SNColourNum];
+
+  /* Convert the species and color fields into mass fractions */
+
+  for (dim = 0, size = 1; dim < GridRank; dim++)
+    size *= GridDimension[dim];
+
+  if (ReturnMassFractions)
+    for (n = n0; n < nfield; n++)
+      for (i = 0; i < size; i++)
+	Prim[n][i] /= Prim[iden][i];
+
+  return SUCCESS;
+
+}
---
+++ src/enzo/hydro_rk/Grid_UpdateElectronDensity.C
@@ -0,0 +1,61 @@
+/***********************************************************************
+/
+/  GRID CLASS (CALCULATE ELECTRON DENSITY FROM SPECIES)
+/
+/  written by: John Wise
+/  date:       July, 2009
+/  modified1:
+/
+/
+************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+#include "macros_and_parameters.h"
+#include "typedefs.h"
+#include "global_data.h"
+#include "Fluxes.h"
+#include "GridList.h"
+#include "ExternalBoundary.h"
+#include "TopGridData.h"
+#include "Grid.h"
+
+int FindField(int field, int farray[], int numfields);
+
+int grid::UpdateElectronDensity(void)
+{
+
+  if (ProcessorNumber != MyProcessorNumber)
+    return SUCCESS;
+
+  if (NumberOfBaryonFields == 0)
+    return SUCCESS;
+
+  if (MultiSpecies == 0)
+    return SUCCESS;
+
+  int i, n, dim, size, nfield, n0;
+  int DeNum, HINum, HIINum, HeINum, HeIINum, HeIIINum, HMNum, H2INum, H2IINum,
+      DINum, DIINum, HDINum;
+
+  this->IdentifySpeciesFields(DeNum, HINum, HIINum, HeINum, HeIINum, HeIIINum, 
+			      HMNum, H2INum, H2IINum, DINum, DIINum, HDINum);
+
+  for (dim = 0, size = 1; dim < GridRank; dim++)
+    size *= GridDimension[dim];
+  
+  for (i = 0; i < size; i++)
+    BaryonField[DeNum][i] = BaryonField[HIINum][i] + BaryonField[HeIINum][i] +
+      2*BaryonField[HeIIINum][i];
+
+  if (MultiSpecies > 1)
+    for (i = 0; i < size; i++)
+      BaryonField[DeNum][i] += BaryonField[H2IINum][i] - BaryonField[HMNum][i];
+
+  if (MultiSpecies > 2)
+    for (i = 0; i < size; i++)
+      BaryonField[DeNum][i] += BaryonField[DIINum][i];
+
+  return SUCCESS;
+
+}
