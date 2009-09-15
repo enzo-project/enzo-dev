05fbe52d03029935b3752cfe532015620e9817b6
Elizabeth Harper-Clark <h-clark@cita.utoronto.ca>
2009-09-15 19:59:34 -0400

    uncommented delete commands
    --HG--
    branch : week-of-code

--- src/enzo/hydro_rk/Grid_MHDTurbulenceInitializeGrid.C
+++ src/enzo/hydro_rk/Grid_MHDTurbulenceInitializeGrid.C
@@ -217,9 +217,9 @@ int grid::MHDTurbulenceInitializeGrid(float rho_medium, float cs_medium, float m
     }
   }
   // printf("Grid_MHDTurb: line 218\n");
-	  /*  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < 3; i++) {
     delete [] TurbulenceVelocity[i];
-    }*/
+    }
 	  // printf("Grid_MHDTurb: line 222\n");
 
   /* Initialize driving force field = efficiency * density * velocity / t_ff*/
@@ -278,9 +278,9 @@ int grid::MHDTurbulenceInitializeGrid(float rho_medium, float cs_medium, float m
     }
 
 
-    /*    for (int dim = 0; dim < GridRank; dim++) {
+     for (int dim = 0; dim < GridRank; dim++) {
       delete [] DrivingField[dim];
-      }*/
+      }
   }    
 
   //printf("Grid_MHDTurb: COMPLETED\n");
