3862b2382684911e74a008a9ba2c2172090ed2c1
Elizabeth Harper-Clark <h-clark@cita.utoronto.ca>
2009-12-09 12:06:49 -0500

    Added multiply by appropriate units in Write parameter file for all parameters divided by units in ReadParameter - Thus when restart things are not divided by unitas AGAIN. This is only for UsePhysicalUnit = 1
    --HG--
    branch : week-of-code

--- src/enzo/WriteParameterFile.C
+++ src/enzo/WriteParameterFile.C
@@ -47,6 +47,9 @@ int WritePhotonSources(FILE *fptr, FLOAT CurrentTime);
 int WriteParameterFile(FILE *fptr, TopGridData &MetaData)
 {
  
+
+
+
   int dim;
  
   /* Compute Units. */
@@ -65,6 +68,28 @@ int WriteParameterFile(FILE *fptr, TopGridData &MetaData)
   if (UsePhysicalUnit) {
     GetUnits(&rhou, &lenu, &tempu, &tu, &velu, &massu, MetaData.Time);
     presu = rhou*lenu*lenu/tu/tu;
+
+    /* Change input physical parameters into real units */
+
+    StarMakerOverDensityThreshold *= rhou;
+    //  StarEnergyFeedbackRate = StarEnergyFeedbackRate/pow(LengthUnits,2)*pow(TimeUnits,3);
+    
+    if (SinkMergeDistance > 1.0)
+      SinkMergeDistance *= lenu;
+    //printf(" \n SinkMergeDistance = %"FSYM"\n \n", SinkMergeDistance);
+    SmallRho *= rhou;
+    SmallP *= presu;
+    SmallT *= tempu;
+    MaximumAlvenSpeed *= velu;
+    EOSSoundSpeed *=  velu;
+    for (int i = 0; i < MAX_FLAGGING_METHODS; i++) {
+      if (MinimumMassForRefinement[i] != FLOAT_UNDEFINED) {
+	MinimumMassForRefinement[i] *= massu;
+      }
+    }
+
+
+
   }
 
   /* write data to Parameter output file */
