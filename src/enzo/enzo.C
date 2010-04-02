99958c9b12708138fba0bc71977f93286121fc9f
John Wise <jwise@astro.princeton.edu>
2010-04-02 09:57:15 -0400

    Made running only the halo finder a command line option (-F).
    --HG--
    branch : enzo-2.0

--- src/enzo/enzo.C
+++ src/enzo/enzo.C
@@ -116,7 +116,8 @@ int InterpretCommandLine(int argc, char *argv[], char *myname,
 			 int RegionStart[], int RegionEnd[],
 			 FLOAT RegionStartCoordinates[],
 			 FLOAT RegionEndCoordinates[],
-			 int &Level, int MyProcessorNumber);
+			 int &Level, int &HaloFinderOnly, 
+			 int MyProcessorNumber);
 void AddLevel(LevelHierarchyEntry *Array[], HierarchyEntry *Grid, int level);
 int SetDefaultGlobalValues(TopGridData &MetaData);
 
@@ -179,6 +180,8 @@ void DeleteGridHierarchy(HierarchyEntry *GridEntry);
 
 void CommunicationAbort(int);
 int ENZO_OptionsinEffect(void);
+int FOF(TopGridData *MetaData, LevelHierarchyEntry *LevelArray[], 
+	int WroteData=1);
 
 #ifdef TASKMAP
 int GetNodeFreeMemory(void);
@@ -313,6 +316,7 @@ Eint32 main(Eint32 argc, char *argv[])
   int restart                  = FALSE,
     OutputAsParticleDataFlag = FALSE,
     InformationOutput        = FALSE,
+    HaloFinderOnly           = FALSE,
     project                  = FALSE,
     ProjectionDimension      = INT_UNDEFINED,
     ProjectionSmooth         = FALSE,
@@ -406,7 +410,8 @@ Eint32 main(Eint32 argc, char *argv[])
 			   &ParameterFile,
 			   RegionStart, RegionEnd,
 			   RegionStartCoordinates, RegionEndCoordinates,
-			   RegionLevel, MyProcessorNumber) == FAIL) {
+			   RegionLevel, HaloFinderOnly,
+			   MyProcessorNumber) == FAIL) {
     if(int_argc==1){
       my_exit(EXIT_SUCCESS);
     } else {
@@ -531,7 +536,12 @@ Eint32 main(Eint32 argc, char *argv[])
     } // ENDFOR dim
   } 
  
-
+  if (HaloFinderOnly) {
+    InlineHaloFinder = TRUE;
+    HaloFinderSubfind = TRUE;
+    FOF(&MetaData, LevelArray);
+    my_exit(EXIT_SUCCESS);
+  }
 
   /* Do vector analysis */
   
