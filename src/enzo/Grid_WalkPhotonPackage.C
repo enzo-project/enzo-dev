6dd1eb8697caf4aa1ae762bf3df3d47c5a7570b7
John Wise <jwise@astro.princeton.edu>
2010-05-19 19:32:49 -0400

    Put nearest edge for geometric correction in a loop.
    --HG--
    branch : week-of-code

--- src/enzo/Grid_WalkPhotonPackage.C
+++ src/enzo/Grid_WalkPhotonPackage.C
@@ -460,10 +460,10 @@ int grid::WalkPhotonPackage(PhotonPackageEntry **PP,
        not completely cover the cell */
 
     midpoint = oldr + 0.5f*ddr - PFLOAT_EPSILON;
-    nearest_edge = -1e20;
     for (dim = 0; dim < 3; dim++)
       m[dim] = fabs(s[dim] + midpoint * u[dim] - (ce[dim] + dxhalf));
-    nearest_edge = max(max(m[0], m[1]), m[2]);
+    for (dim = 1, nearest_edge = m[0]; dim < 3; dim++)
+      if (m[dim] > nearest_edge) nearest_edge = m[dim];
     sangle_inv = 1.0 / (dtheta*radius);
     slice_factor = min(0.5f + (dxhalf-nearest_edge) * sangle_inv, 1.0f);
     slice_factor2 = slice_factor * slice_factor;
