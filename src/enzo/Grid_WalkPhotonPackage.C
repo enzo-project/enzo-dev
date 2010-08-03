b370afbcf8887db2aa839ca94073f8896e94d0df
John Wise <jwise@astro.princeton.edu>
2010-08-03 11:52:28 -0400

    No hard fail when ray is out of the grid (happens very rarely. only by one cell, and I don't know why).  Just warn the user.
    --HG--
    branch : euler_improve

--- src/enzo/Grid_WalkPhotonPackage.C
+++ src/enzo/Grid_WalkPhotonPackage.C
@@ -152,8 +152,12 @@ int grid::WalkPhotonPackage(PhotonPackageEntry **PP,
     // Current cell in integer and floating point
     g[dim] = GridStartIndex[dim] + 
       nint(floor((r[dim] - GridLeftEdge[dim]) / CellWidth[dim][0]));
-    if (g[dim] < 0 || g[dim] >= GridDimension[dim])
-      ENZO_FAIL("Ray out of grid?");
+    if (g[dim] < 0 || g[dim] >= GridDimension[dim]) {
+      printf("Ray out of grid? g = %d %d %d\n", g[0], g[1], g[2]);
+      DeleteMe = TRUE;
+      return SUCCESS;
+      //ENZO_FAIL("Ray out of grid?");
+    }
     f[dim] = CellLeftEdge[dim][g[dim]];
 
     // On cell boundaries, the index will change in negative directions
