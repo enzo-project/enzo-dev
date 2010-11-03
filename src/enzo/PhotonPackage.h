217d272ca3bee841b3f249b6d39f4f8c09348b59
Britton Smith <brittonsmith@gmail.com>
2010-11-03 11:22:01 -0400

    Fixed some print statements.
    --HG--
    branch : new_problem_types

--- src/enzo/PhotonPackage.h
+++ src/enzo/PhotonPackage.h
@@ -54,9 +54,9 @@ public:
     pix2vec_nest((long)(1 << level), ipix, u);
     for (int dim = 0; dim < 3; dim++) 
       r[dim] = SourcePosition[dim] + u[dim] * Radius;
-    printf("Photons = %g, Type = %d, Radius = %"PSYM"\n", Photons, Type, Radius);
-    printf("ipix = %ld, level = %d\n", ipix, level);
-    printf("normal = %lf %lf %lf\n", u[0], u[1], u[2]);
+    printf("Photons = %g, Type = %"ISYM", Radius = %"PSYM"\n", Photons, Type, Radius);
+    printf("ipix = %ld, level = %"ISYM"\n", ipix, level);
+    printf("normal = %"PSYM" %"PSYM" %"PSYM"\n", u[0], u[1], u[2]);
     printf("SourcePosition = %"PSYM" %"PSYM" %"PSYM"\n",
 	   SourcePosition[0], SourcePosition[1], SourcePosition[2]);
     printf("RayPosition = %"PSYM" %"PSYM" %"PSYM"\n", r[0], r[1], r[2]);
