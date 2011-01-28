d1618bb049b300ab48b7a16028bb5157b742bf10
Matthew Turk <matthewturk@gmail.com>
2011-01-28 13:45:24 -0500

    Adding particle_type to inline Enzo
    --HG--
    branch : euler_improve

--- src/enzo/Grid_ConvertToNumpy.C
+++ src/enzo/Grid_ConvertToNumpy.C
@@ -148,6 +148,14 @@ void grid::ConvertToNumpy(int GridID, PyArrayObject *container[], int ParentID, 
               (PyObject*) dataset);
           Py_DECREF(dataset);
 
+          /* Type */
+          dataset = (PyArrayObject *) PyArray_SimpleNewFromData(
+                  1, dims, ENPY_INT, ParticleType);
+          dataset->flags &= ~NPY_OWNDATA;
+          PyDict_SetItemString(grid_data, "particle_type",
+              (PyObject*) dataset);
+          Py_DECREF(dataset);
+
         }
 
         grid_id = PyLong_FromLong((long) GridID);
