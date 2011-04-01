2b78b137a4dba81a112b0784ab38c9de501bcd88
Matthew Turk <matthewturk@gmail.com>
2011-04-01 16:48:06 -0400

    Some fixes and adding optparse
    --HG--
    branch : week-of-code

--- src/test_runner/test_finder.py
+++ src/test_runner/test_finder.py
@@ -1,23 +1,33 @@
 # This is the object that locates all *.enzo_test directories
 
 import os.path
+import optparse
+import sys
 
-known_variables = dict(
-    name = str,
-    answer_testing_script = str,
-    nprocs = int,
-    runtime = str,
-    critical = bool,
-    cadence = str,
-    hydro = bool,
-    mhd = bool,
-    gravity = bool,
-    cosmology = bool,
-    AMR = bool,
-    dimensionality = int,
-    fullpath = str,
-    fulldir = str
+varspec = dict(
+    name = (str, ''),
+    answer_testing_script = (str, None),
+    nprocs = (int, 1),
+    runtime = (str, 'short'),
+    critical = (bool, True),
+    cadence = (str, 'nightly'),
+    hydro = (bool, False),
+    mhd = (bool, False),
+    gravity = (bool, False),
+    cosmology = (bool, False),
+    chemistry = (bool, False),
+    cooling = (bool, False),
+    AMR = (bool, False),
+    dimensionality = (int, 1),
+    author = (str, ''),
+    fullpath = (str, '.'),
+    fulldir = (str, '.'),
+    max_time_minutes = (float, 1),
+    radiation = (str, None),
 )
+
+known_variables = dict( [(k, v[0]) for k, v in varspec.items()] )
+variable_defaults = dict( [(k, v[1]) for k, v in varspec.items()] )
 
 def add_files(my_list, dirname, fns):
     my_list += [os.path.join(dirname, fn) for
@@ -41,11 +51,13 @@ class EnzoTestCollection(object):
         # its environment variables from it.
         local_vars = {}
         execfile(fn, {}, local_vars)
-        test_spec = dict(fullpath = fn,
-                         fulldir = os.path.dirname(fn))
+        test_spec = variable_defaults.copy()
+        test_spec['fullpath'] = fn
+        test_spec['fulldir'] = os.path.dirname(fn)
         for var, val in local_vars.items():
             if var in known_variables:
                 caster = known_variables[var]
+                if val == "False": val = False
                 test_spec[var] = caster(val)
                 if val == "None": test_spec[var] = None
             else:
@@ -66,11 +78,15 @@ class EnzoTestCollection(object):
 
     def select(self, **kwargs):
         pp = []
-        for param, value in kwargs.items():
-            if value == "None": value = None
-            for t in self.tests:
-                if t.get(param, "Key Missing") == value:
-                    pp.append(t)
+        for t in self.tests:
+            include = True
+            for param, value in kwargs.items():
+                if value == "None": value = None
+                if value == "False": value = False
+                if t.get(param, "Key Missing") != value:
+                    include = False
+                    break
+            if include == True: pp.append(t)
         return EnzoTestCollection(tests = pp)
 
     def summary(self):
@@ -83,6 +99,31 @@ class EnzoTestCollection(object):
         print
         print "NUMBER OF TESTS", len(self.tests)
 
+class UnspecifiedParameter(object):
+    pass
+unknown = UnspecifiedParameter()
+
 if __name__ == "__main__":
     etc = EnzoTestCollection()
-    etc2 = etc.select(runtime="long")
+    parser = optparse.OptionParser()
+    parser.add_option("-o", "--output-dir", dest='outputdir',
+                      help="Where to place the run directory")
+    for var, caster in sorted(known_variables.items()):
+        parser.add_option("", "--%s" % (var),
+                          type=str, default = unknown)
+    options, args = parser.parse_args()
+    construct_selection = {}
+    for var, caster in known_variables.items():
+        if getattr(options, var) != unknown:
+            val = getattr(options, var)
+            if val == 'None': val = None
+            if val == "False": val = False
+            construct_selection[var] = caster(val)
+    print
+    print "Selecting with:"
+    for k, v in sorted(construct_selection.items()):
+        print "     %s = %s" % (k, v)
+    etc2 = etc.select(**construct_selection)
+    print
+    print "\n".join(list(etc2.unique('name')))
+    print "Total: %s" % len(etc2.tests)
