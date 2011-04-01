19cd7057d7a348c8abb9f0e0d1b8153b8d5d1d77
Matthew Turk <matthewturk@gmail.com>
2011-04-01 15:41:18 -0400

    Initial pass at the test finder
    --HG--
    branch : week-of-code

--- src/test_runner/test_finder.py
+++ src/test_runner/test_finder.py
@@ -1,1 +1,44 @@
 # This is the object that locates all *.enzo_test directories
+
+import glob
+
+known_variables = dict(
+    name = str,
+    answer_testing_script = str,
+    nprocs = int,
+    runtime = str,
+    critical = bool,
+    cadence = str,
+    hydro = bool,
+    mhd = bool,
+    gravity = bool,
+    cosmology = bool,
+    AMR = bool,
+    dimensionality = int
+)
+
+class EnzoTestCollection(object):
+    def __init__(self):
+        # Now we look for all our *.enzo_test files
+        fns = glob.glob("**/*.enzo_test")
+        self.tests = []
+        for fn in sorted(fns):
+            print "HANDLING", fn
+            self.add_test(fn)
+
+    def add_test(self, fn):
+        # We now do something dangerous: we exec the file directly and grab
+        # its environment variables from it.
+        local_vars = {}
+        execfile(fn, {}, local_vars)
+        test_spec = {}
+        for var, val in local_vars.items():
+            if var in known_variables:
+                caster = known_variables[var]
+                test_spec[var] = caster(val)
+            else:
+                print "%s UNRECOGNIZED VARIABLE %s" % ( fn, var)
+        self.tests.append(test_spec)
+
+if __name__ == "__main__":
+    etc = EnzoTestCollection()
