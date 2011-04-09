8d06d579fa8f0f94cd9816bc724477538b8a9e60
John Wise <jwise@astro.princeton.edu>
2011-04-09 17:34:31 -0400

    Fixing memory leak.  Baryons should be deposited only on the host processor.
    --HG--
    branch : week-of-code

--- src/enzo/DepositBaryons.C
+++ src/enzo/DepositBaryons.C
@@ -84,10 +84,12 @@ int DepositBaryonsChildren(HierarchyEntry *DepositGrid,
   if (CommunicationDirection == COMMUNICATION_SEND ||
       CommunicationDirection == COMMUNICATION_SEND_RECEIVE) {
     HierarchyEntry *Temp = Grid->NextGridNextLevel;
-    Grid->GridData->ZeroSolutionUnderSubgrid(NULL, ZERO_UNDER_SUBGRID_FIELD, 1., 1, 1);
+    Grid->GridData->ZeroSolutionUnderSubgrid(NULL, ZERO_UNDER_SUBGRID_FIELD, 
+					     1.0, FALSE, TRUE);
     while (Temp != NULL) {
       Grid->GridData->ZeroSolutionUnderSubgrid(Temp->GridData,
-					       ZERO_UNDER_SUBGRID_FIELD, 1., 1, 1);
+					       ZERO_UNDER_SUBGRID_FIELD,
+					       1.0, FALSE, TRUE);
       Temp = Temp->NextGridThisLevel;
     }
   }
