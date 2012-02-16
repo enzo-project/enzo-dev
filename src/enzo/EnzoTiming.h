1114e603b3a06e63e3b0fa45fd1b075bd79cc658
Sam Skillman <sam.skillman@gmail.com>
2012-02-16 14:42:53 -0700

    Documenting.
    --HG--
    branch : week-of-code

--- src/enzo/EnzoTiming.h
+++ src/enzo/EnzoTiming.h
@@ -1,3 +1,21 @@
+/***********************************************************************
+/
+/  EnzoTiming.h  
+/
+/  written by: Samuel Skillman
+/  date:       February, 2012
+/  modified1:
+/
+/  PURPOSE: The framework for lightweight timing functions for Enzo 
+/   Routines. Sets up the enzo_timing namespace, which contains the 
+/   following classes:
+/       section_performance: Contains timing information for a single 
+/           code section
+/       enzo_timer: Contains general information and section_performance 
+/           objects.
+/
+************************************************************************/
+
 #ifndef ENZO_TIMING__
 #define ENZO_TIMING__
 
@@ -17,11 +35,17 @@ double ReturnWallTime(void);
 void Reduce_Times(double time, double *time_array);
 
 namespace enzo_timing{
-  
+
+  // Section Performance Class
+  // Contains timers, stop/start functions, and accessors. 
+  // The main things kept track of are current and total times.
+  // Current is meant to be reset to 0.0 every time the timer 
+  // is written out, whereas total is kept until the simulation 
+  // ends.
   class section_performance
   {
   public:
-    
+    // Constructor 
     section_performance(char* myname){
       name = myname;
       next = NULL;
@@ -30,60 +54,75 @@ namespace enzo_timing{
       total_time = 0.0;
       current_time = 0.0;
     }
-    
-    std::string name;
-
+   
+    // Start Timer 
     void start(void){
       t0 = ReturnWallTime();
     }
-
+  
+    // Stop Timer, add to current/total times
     void stop(void){
       t1 = ReturnWallTime();
       current_time += t1-t0;
       total_time += t1-t0;
     }
 
+    // Access total_time
     double get_total_time(void){
       return total_time;
     }
+    
+    // Access current_time
     double get_current_time(void){
       return current_time;
     }
+
+    // Reset the current_time timer.
     void reset_current_time(void){
       current_time = 0.0;
     }
 
+    // Access the ncells counter
     long int get_cells(void){
       return ncells;
     }
+
+    // Access the ngrids counter
     long int get_grids(void){
       return ngrids;
     }
 
+    // Add the number of grids, and cells to this timer.
     void add_stats(long int my_grids, long int my_cells){
       ngrids = my_grids;
       ncells = my_cells;
     }
 
-    section_performance *next;
+    std::string name;           // Name of the timer
+    section_performance *next;  // Pointer to the next timer 
   
   private:
-    double t0;
-    double t1;
-    double total_time;
-    double current_time;
-    long int ngrids;
-    long int ncells;
+    double t0;            // Start Time
+    double t1;            // End Time
+    double total_time;    // Total time during the simulation
+    double current_time;  // Time spent in this timer since last write-out
+    long int ngrids;      // Number of Grids (For Level Timers)
+    long int ncells;      // Number of Cells (For Level Timers)
     
   };
 
 
   /* --------------------------------------------------------- */
 
+  // enzo_timer class definition
+  // Contains linked list of timers, helper functions to 
+  // start/stop timers, write out the data, and manage the 
+  // timers. 
   class enzo_timer
   {
   public:
 
+    // Constructor, uses default filename.
     enzo_timer(void){
       total_time = 0.0;
       current_time = 0.0;
@@ -91,6 +130,7 @@ namespace enzo_timing{
       set_mpi_environment();
     }
 
+    // Constructor, accepts non-standard filename
     enzo_timer(char *performance_name){
       total_time = 0.0;
       current_time = 0.0;
@@ -98,6 +138,7 @@ namespace enzo_timing{
       set_mpi_environment();
     }
 
+    // Destructor, erases each timer.
     ~enzo_timer(void){
       for( iter=timers.begin(); iter!=timers.end(); ++iter){
         delete [] iter->second;
@@ -105,6 +146,8 @@ namespace enzo_timing{
       }
       timers.clear();
     }
+
+    // Sets up nprocs/my_rank properly.
     void set_mpi_environment(void){
 #ifdef USE_MPI
       MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
@@ -115,9 +158,12 @@ namespace enzo_timing{
 #endif
     }
 
+    // Use std::map objects and iterators to store timers
+    // in a linked list.
     std::map<std::string, section_performance *> timers;
     std::map<std::string, section_performance *>::iterator iter;
 
+    // Accessor for section performance object
     section_performance * get(char *name){
       iter = timers.find(name);
       if (iter == timers.end()){
@@ -127,6 +173,7 @@ namespace enzo_timing{
       return timers[name];
     }
 
+    // Start a timer by name
     void start(char *name){
       iter = timers.find(name);
       if (iter == timers.end()){
@@ -136,6 +183,7 @@ namespace enzo_timing{
       timers[name]->start();
     }
 
+    // Stop a timer by name
     void stop(char *name){
       iter = timers.find(name);
       if (iter == timers.end()){
@@ -145,12 +193,14 @@ namespace enzo_timing{
       timers[name]->stop();
     }
 
+    // Get a level section_performance by level
     section_performance * get_level(int level){
       char level_name[256];
       sprintf(level_name, "Level_%d", level);
       return get(level_name);
     }
 
+    // Access total_time by summing from timers
     double get_total_time(void){
       total_time = 0.0;
       for( iter=timers.begin(); iter!=timers.end(); ++iter){
@@ -159,6 +209,7 @@ namespace enzo_timing{
       return total_time;
     }
 
+    // Get the total time for the level timers.
     double get_total_levels_time(void){
       total_time = 0.0;
       std::string keyname;
@@ -171,6 +222,7 @@ namespace enzo_timing{
       return total_time;
     }
 
+    // Not implemented.
     int get_total_grids(void){
       return 0;
       /* temp_performance = level_zero_performance; */
@@ -183,6 +235,7 @@ namespace enzo_timing{
       /* return total_grids; */
     }
 
+    // Get sum of current_time's
     double get_current_time(void){
       current_time = 0.0;
       for( iter=timers.begin(); iter!=timers.end(); ++iter){
@@ -191,6 +244,7 @@ namespace enzo_timing{
       return current_time;
     }
 
+    // Get sum of level current_time's
     double get_current_levels_time(void){
       total_time = 0.0;
       std::string keyname;
@@ -203,6 +257,8 @@ namespace enzo_timing{
       return total_time;
     }
 
+    // Write out performance measures to a file, optionally specifying
+    // verbose to get all timers from all processors.
     void write_out(int step, bool verbose=false){
       if (my_rank == 0){
         chronos_file = fopen(filename,"a");
@@ -220,9 +276,12 @@ namespace enzo_timing{
       }
       std::string keyname;
 
+      // Collect times from all processors.
       double thistime = this->get_current_levels_time();
       Reduce_Times(thistime, time_array);
 
+      // Write out Cycle Number and calculate stats on total time
+      // for this cycle.
       double mean_time = 0.0;
       double min_time, max_time, stddev_time;
       if (my_rank == 0){
@@ -245,6 +304,7 @@ namespace enzo_timing{
       
       long int total_cells = 0;
       
+      // Print out info for each timer.
       for( iter=timers.begin(); iter!=timers.end(); ++iter){
         current_time = iter->second->get_current_time();
         iter->second->reset_current_time();
@@ -288,6 +348,7 @@ namespace enzo_timing{
         }
       }
 
+      // Write out total cells divided by processor-seconds.
       if (my_rank == 0){
         fprintf(chronos_file, "Total_Cells/Sec/Processor %e\n", (double)(total_cells/total_time/nprocs));
         fprintf(chronos_file, "\n");
@@ -297,12 +358,12 @@ namespace enzo_timing{
     }
           
   private:
-    FILE * chronos_file;
-    double total_time;
-    double current_time;
-    char * filename;
-    int my_rank;
-    int nprocs;
+    FILE * chronos_file;    // File to write stats to
+    double total_time;      // Total time
+    double current_time;    // Current Time since last write_out
+    char * filename;        // Filename
+    int my_rank;            // MPI Rank
+    int nprocs;             // MPI Size
   };
 }
 
@@ -312,8 +373,9 @@ namespace enzo_timing{
 # define EXTERN extern
 #endif
 
-EXTERN enzo_timing::enzo_timer *enzo_timer;                                                         
+EXTERN enzo_timing::enzo_timer *enzo_timer;   // Add global timer                                                         
 
+// Macros to start, stop, and write timers.
 #define TIMING_ON
 #ifdef TIMING_ON
 #define TIMER_START(section_name) enzo_timer->start(section_name)
