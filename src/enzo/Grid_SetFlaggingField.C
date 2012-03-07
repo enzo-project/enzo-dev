4aee12a902126be5388960a3d95a410dcbaf31d4
John Wise <jwise@physics.gatech.edu>
2012-03-07 13:27:31 -0500

    Level loop in RH over all levels and then check if grids exist.
    --HG--
    branch : week-of-code

--- src/enzo/Grid_SetFlaggingField.C
+++ src/enzo/Grid_SetFlaggingField.C
@@ -255,6 +255,10 @@ int grid::SetFlaggingField(int &NumberOfFlaggedCells, int level)
  
   }
 
+  if (debug1 && NumberOfFlaggedCells > 0)
+    printf("SetFlaggingField[method = %"ISYM"]: NumberOfFlaggedCells = %"ISYM".\n",
+	   CellFlaggingMethod[method], NumberOfFlaggedCells);
+
   } // ENDFOR methods
  
   /* End of Cell flagging criterion routine                              */
@@ -268,12 +272,7 @@ int grid::SetFlaggingField(int &NumberOfFlaggedCells, int level)
   counter[4]++;
   timer[4] += NumberOfFlaggedCells;
 #endif /* MPI_INSTRUMENTATION */
- 
-  if (debug1)
-
-    printf("SetFlaggingField[method = %"ISYM"]: NumberOfFlaggedCells = %"ISYM".\n",
-	   method, NumberOfFlaggedCells);
- 
+  
   return SUCCESS;
  
 }
