49024dc13e6f15ed654ee7e425c589da13ee026d
John Wise <jwise@physics.gatech.edu>
2012-04-06 17:15:27 -0400

    Extending optically-thin X-rays to stars.
    --HG--
    branch : week-of-code

--- src/enzo/Grid_AddXraysFromSources.C
+++ src/enzo/Grid_AddXraysFromSources.C
@@ -149,7 +149,7 @@ int grid::AddXraysFromSources(Star *AllStars)
 
 	for (a = 0; a < 3; a++) {
 	  kph_r2 = (float) (XrayLuminosity * CrossSections[a] / (4.0 * M_PI));
-	  dE = (RS->SED[bin] - EnergyThresholds[a]);
+	  dE = (RS->Energy[bin] - EnergyThresholds[a]);
 	  for (k = 0; k < ActiveDims[2]; k++) {
 	    for (j = 0; j < ActiveDims[1]; j++) {
 	      radius2_yz = ddr2[1][j] + ddr2[2][k];
@@ -181,7 +181,6 @@ int grid::AddXraysFromSources(Star *AllStars)
 
   else {
 
-#ifdef UNUSED
     for (cstar = AllStars; cstar; cstar = cstar->NextStar) {
 
       // Skip if not 'living'
@@ -189,12 +188,11 @@ int grid::AddXraysFromSources(Star *AllStars)
 	    cstar->FeedbackFlag == CONT_SUPERNOVA)) 
 	continue;
       
-      /* Determine H2 emission rate */
+      /* Get energy bins and SED */
 
       if (cstar->ComputePhotonRates(nbins, energies, Luminosity) == FAIL) {
 	ENZO_FAIL("Error in ComputePhotonRates.\n");
       }
-      H2Luminosity = Luminosity[3];
 
       /* Pre-calculate distances from cells to source */
 
@@ -210,32 +208,68 @@ int grid::AddXraysFromSources(Star *AllStars)
 	  ddr2[dim][i] = ddr2[dim][i] * ddr2[dim][i];
 	}
 
-      /* Loop over cells */
+      /* Loop over energy bins and consider X-rays to be E >= 100 eV */
 
-      double radius2, radius2_yz;
+      for (bin = 0; bin < nbins; bin++) {
 
-      kdiss_r2 = (float) (H2Luminosity * H2ISigma / (4.0 * M_PI));
-      for (k = 0; k < ActiveDims[2]; k++) {
-	for (j = 0; j < ActiveDims[1]; j++) {
-	  radius2_yz = ddr2[1][j] + ddr2[2][k];
-	  index = GRIDINDEX(0, j, k);
-	  for (i = 0; i < ActiveDims[0]; i++, index++) {
-	    radius2 = radius2_yz + ddr2[0][i];
-	    //if (radius2 < outerFront2 && radius2 > innerFront2) {
-	    //radius2 = max(radius2, dilRadius2);
-	    if (radius2 < dilRadius2)
+	if (energies[bin] < 100) continue;
 
-	      BaryonField[kdissH2INum][index] += kdiss_r2 / dilRadius2;
-	    else
-	      BaryonField[kdissH2INum][index] += kdiss_r2 / radius2;
-	    //} // ENDIF
-	  } // END: i-direction
-	} // END: j-direction
-      } // END: k-direction
+	XrayLuminosity = Luminosity[bin] / LConv;
+	for (i = 0; i < 3; i++)
+	  CrossSections[i] = FindCrossSection(i, energies[bin]) * CrossSectionConv;
+
+	if (RadiationXRaySecondaryIon == TRUE) {
+	  nSecondaryHII = energies[bin] / 13.6;
+	  nSecondaryHeII = energies[bin] / 24.6;
+	}
+
+	/* Pre-calculate distances from cells to source */
+
+	for (dim = 0; dim < GridRank; dim++)
+	  for (i = 0, index = GridStartIndex[dim]; i < ActiveDims[dim]; 
+	       i++, index++) {
+
+	    // Calculate dr_i first, then square it
+	    ddr2[dim][i] = 
+	      fabs(CellLeftEdge[dim][index] + 0.5*CellWidth[dim][index] - 
+		   cstar->pos[dim]);
+	    ddr2[dim][i] = min(ddr2[dim][i], DomainWidth[dim]-ddr2[dim][i]);
+	    ddr2[dim][i] = ddr2[dim][i] * ddr2[dim][i];
+	  }
+
+	/* Loop over absorbers then cells */
+
+	double radius2_yz;
+
+	for (a = 0; a < 3; a++) {
+	  kph_r2 = (float) (XrayLuminosity * CrossSections[a] / (4.0 * M_PI));
+	  dE = (energies[bin] - EnergyThresholds[a]);
+	  for (k = 0; k < ActiveDims[2]; k++) {
+	    for (j = 0; j < ActiveDims[1]; j++) {
+	      radius2_yz = ddr2[1][j] + ddr2[2][k];
+	      index = GRIDINDEX(0, j, k);
+	      for (i = 0; i < ActiveDims[0]; i++, index++) {
+		dkph = kph_r2 / (radius2_yz + ddr2[0][i]);
+		if (RadiationXRaySecondaryIon == TRUE) {
+		  xx = max(BaryonField[HIINum][index] / 
+			   (BaryonField[HINum][index] + BaryonField[HIINum][index]),
+			   1e-4);
+		  heat_factor = 0.9971 * (1.0 - powf(1.0 - powf(xx, 0.2663f), 1.3163));
+		  ion2_factor[0] = 0.3908 * nSecondaryHII * 
+		    powf(1 - powf(xx, 0.4092f), 1.7592f);
+		  ion2_factor[1] = 0.0554 * nSecondaryHeII * 
+		    powf(1 - powf(xx, 0.4614f), 1.6660f);
+		}
+		BaryonField[kphNum[a]][index] += dkph * ion2_factor[a];
+		BaryonField[gammaNum][index] += dkph * dE * heat_factor;
+	      } // END: i-direction
+	    } // END: j-direction
+	  } // END: k-direction
+	} // END: absorbers (a)
+
+      } // ENDFOR bin
+      
     } // ENDFOR stars
-#else /* UNUSED */
-    ENZO_FAIL("Optically thin X-rays not implemented for stars yet.");
-#endif
 
   } // ENDELSE ProblemType == 50
 
