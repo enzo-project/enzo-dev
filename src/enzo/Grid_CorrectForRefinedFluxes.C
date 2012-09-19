6c44ea3ebbd726b00b303d6380cff4e2e1fff70c
David Collins (dcollins4096@gmail.com) <David Collins (dcollins4096@gmail.com)>
2012-09-19 10:33:37 -0600

    Adding functionality that generalizes and simplifies the conversion from specific to conserved quantities
    --HG--
    branch : week-of-code

--- src/enzo/Grid_CorrectForRefinedFluxes.C
+++ src/enzo/Grid_CorrectForRefinedFluxes.C
@@ -192,15 +192,10 @@ int grid::CorrectForRefinedFluxes(fluxes *InitialFluxes,
  
 	if (HydroMethod != Zeus_Hydro)
 	  for (field = 0; field < NumberOfBaryonFields; field++)
-	    if (FieldTypeIsDensity(FieldType[field]) == FALSE &&
-		FieldTypeIsRadiation(FieldType[field]) == FALSE &&
-		FieldType[field] != Bfield1 &&
-		FieldType[field] != Bfield2 && FieldType[field] != Bfield3 &&
-		FieldType[field] != PhiField &&
-		FieldType[field] != DrivingField1 &&
-		FieldType[field] != DrivingField2 &&
-		FieldType[field] != DrivingField3 &&
-		FieldType[field] != GravPotential)  
+	    if (
+                MakeFieldConservative(FieldType[field])
+           )
+                
 	      //		(RadiativeCooling == 0 || (FieldType[field] != TotalEnergy &&
 	      //	 			 FieldType[field] != InternalEnergy)))
 	      for (k = Start[2]; k <= End[2]; k++)
@@ -338,13 +333,8 @@ int grid::CorrectForRefinedFluxes(fluxes *InitialFluxes,
 	  for (field = 0; field < NumberOfBaryonFields; field++)
 	    if (FieldTypeIsDensity(FieldType[field]) == FALSE &&
 		FieldTypeIsRadiation(FieldType[field]) == FALSE &&
-		FieldType[field] != Bfield1 &&
-		FieldType[field] != Bfield2 && FieldType[field] != Bfield3 &&
-		FieldType[field] != PhiField &&
-		FieldType[field] != DrivingField1 &&
-		FieldType[field] != DrivingField2 &&
-		FieldType[field] != DrivingField3 &&
-		FieldType[field] != GravPotential)
+        MakeFieldConservative(FieldType[field]))
+        
 	      for (k = Start[2]; k <= End[2]; k++)
 		for (j = Start[1]; j <= End[1]; j++) {
 		  index = (k*GridDimension[1] + j)*GridDimension[0] + Start[0];
