729b6e43ce63035df4536f6cccc848b59ed02188
Matthew Turk <matthewturk@gmail.com>
2012-11-07 15:18:22 -0500

    Adding a testing template and a simple script to make lots more of them.
    --HG--
    branch : week-of-code

---
+++ run/make_new_tests.py
@@ -0,0 +1,13 @@
+import shutil, os
+
+template = open("test_type.py.template").read()
+
+for root, dirs, files in os.walk("."):
+    for fn in files:
+        if fn.endswith(".enzo"):
+            simname = os.path.splitext(fn)[0]
+            simpath = root
+            oname = os.path.join(root, "test_standard.py")
+            output = template % dict(simname = simname, filename = fn,
+                                     simpath = simpath)
+            open(oname, "w").write(output)
