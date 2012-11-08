a9a3cc71f1d784c4bae921b873b4c576beddbf0c
Britton Smith <brittonsmith@gmail.com>
2012-11-07 21:02:24 -0500

    Changing test over to use new numpy testing.
    --HG--
    branch : week-of-code

--- run/Hydro/Hydro-3D/CollapseTestNonCosmological/test_collapse.py
+++ run/Hydro/Hydro-3D/CollapseTestNonCosmological/test_collapse.py
@@ -1,36 +1,26 @@
 from yt.mods import *
+from yt.testing import *
 from yt.utilities.answer_testing.api import YTStaticOutputTest
-
-class TestCollapseProjection(YTStaticOutputTest):
-    name = "collapse_projection"
-
-    def run(self):
-        # self.pf already exists
-        proj = self.pf.h.proj(0, 'Density', center=[0.5, 0.5, 0.5])
-        frb = FixedResolutionBuffer(proj, (0.0, 1.0, 0.0, 1.0), (150, 150))
-        self.result = frb["Density"]
-
-    def compare(self, old_result):
-        current_buffer = self.result
-        old_buffer = old_result
-
-        # We want our arrays to agree to some delta
-        self.compare_array_delta(current_buffer, old_buffer, 5e-3)
-
-    def plot(self):
-        # There's not much to plot, so we just return an empty list.
-        return []
 
 class TestCollapseMaxValue(YTStaticOutputTest):
     name = "collapse_max"
 
+    def __init__(self, sim):
+        self.pf = sim
+    
     def run(self):
-        # self.pf already exists
-        self.result = self.pf.h.find_max("Density")[0]
+        result = []
+        for my_pf in pf:
+            result.append(self.my_pf.h.find_max("Density")[0])
 
-    def compare(self, old_result):
-        self.compare_value_delta(self.result, old_result, 5e-3)
+    def compare(self, new_result, old_result):
+        for i in range(len(new_result)):
+            assert_rel_equal(new_result[i], old_result[i], 2)
 
-    def plot(self):
-        # There's not much to plot, so we just return an empty list.
-        return []
+def test_collapse_max_value():
+    # Create a time series object.
+    sim = simulation('CollapseTestNonCosmological.enzo', 'Enzo',
+                     find_outputs=True)
+    sim.get_time_series()
+    
+    yield TestCollapseMaxValue(sim)
