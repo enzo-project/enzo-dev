f6a0430fafcebcfaa070f30392c28baf6ec235c3
Britton Smith <brittonsmith@gmail.com>
2012-11-07 21:32:07 -0500

    Fixing CollapseTest test.
    --HG--
    branch : week-of-code

--- run/Hydro/Hydro-3D/CollapseTestNonCosmological/test_collapse.py
+++ run/Hydro/Hydro-3D/CollapseTestNonCosmological/test_collapse.py
@@ -1,9 +1,13 @@
 from yt.mods import *
 from yt.testing import *
-from yt.utilities.answer_testing.api import YTStaticOutputTest
+from yt.utilities.answer_testing.api import AnswerTestingTest
+from yt.utilities.answer_testing.framework import \
+    requires_outputlog, \
+    sim_dir_load
 
-class TestCollapseMaxValue(YTStaticOutputTest):
-    name = "collapse_max"
+class TestCollapseMaxValue(AnswerTestingTest):
+    _type_name = "MaxValue"
+    _attrs = ()
 
     def __init__(self, sim):
         self.pf = sim
@@ -17,10 +21,12 @@ class TestCollapseMaxValue(YTStaticOutputTest):
         for i in range(len(new_result)):
             assert_rel_equal(new_result[i], old_result[i], 2)
 
+@requires_outputlog("./Hydro/Hydro-3D/CollapseTestNonCosmological", 
+                    "CollapseTestNonCosmological.enzo"
 def test_collapse_max_value():
-    # Create a time series object.
-    sim = simulation('CollapseTestNonCosmological.enzo', 'Enzo',
-                     find_outputs=True)
+    sim = sim_dir_load("CollapseTestNonCosmological.enzo", 
+                       path="./Hydro/Hydro-3D/CollapseTestNonCosmological", 
+                       find_outputs=True)
     sim.get_time_series()
     
     yield TestCollapseMaxValue(sim)
