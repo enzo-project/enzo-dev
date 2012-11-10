c87dfb6e61059927570033ad7398c00dffb3ad01
Britton Smith <brittonsmith@gmail.com>
2012-11-10 09:47:27 -0500

    Updating FreeExpansion test.
    --HG--
    branch : week-of-code

--- run/Hydro/Hydro-1D/FreeExpansion/test_free_expansion.py
+++ run/Hydro/Hydro-1D/FreeExpansion/test_free_expansion.py
@@ -1,41 +1,19 @@
-from yt.config import ytcfg
-ytcfg["yt","loglevel"] = '50'
-ytcfg["yt","suppressStreamLogging"] = 'True'
-
 from yt.mods import *
-from yt.utilities.answer_testing.api import YTStaticOutputTest
+from yt.testing import *
+from yt.utilities.answer_testing.framework import \
+     FieldValuesTest, \
+     requires_outputlog, \
+     sim_dir_load
 
-class TestFreeExpansion1D(YTStaticOutputTest):
-    name = "expansion_image"
+_fields = ("VelocityMagnitude",)
+_pf_name = os.path.basename(os.path.dirname(__file__)) + ".enzo"
+_dir_name = os.path.dirname(__file__)
 
-    def run(self):
-        # self.pf already exists
-        ray = self.pf.h.ray([0.0,0.5,0.5], [1.0,0.5,0.5])
-        self.result = ray["Density"]
-
-    def compare(self, old_result):
-        current_buffer = self.result
-        old_buffer = old_result
-
-        # We want our arrays to agree to some delta
-        self.compare_array_delta(current_buffer, old_buffer, 5e-3)
-
-    def plot(self):
-        # There's not much to plot, so we just return an empty list.
-        return []
-
-class TestFreeExpansionDistance(YTStaticOutputTest):
-    name = "expansion_dist"
-
-    def run(self):
-        # self.pf already exists
-        ray = self.pf.h.ray([0.0,0.5,0.5], [1.0,0.5,0.5])
-        ipos = na.where(ray['VelocityMagnitude'] == 0.0)[0].argmin()
-        self.result = ray['t'][ipos]
-
-    def compare(self, old_result):
-        self.compare_value_delta(self.result, old_result, 1e-2)
-
-    def plot(self):
-        # There's not much to plot, so we just return an empty list.
-        return []
+@requires_outputlog(_dir_name, _pf_name)
+def test_velocity_megnitude():
+    sim = sim_dir_load(_pf_name, path=_dir_name,
+                       find_outputs=True)
+    sim.get_time_series()
+    for pf in sim:
+        for field in _fields:
+            yield FieldValuesTest(pf, field)
