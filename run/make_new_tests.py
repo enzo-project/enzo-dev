0c3a950845dac7bb7c8f98d62c2034a9e6e385d6
Britton Smith <brittonsmith@gmail.com>
2012-11-14 10:01:24 -0500

    Adding an ignore list to make_new_tests so we can skip tests that write out data that can't be read by yt easily.  Also, changing the test template to only test the last dataset made by the simulation.
    --HG--
    branch : week-of-code

--- run/make_new_tests.py
+++ run/make_new_tests.py
@@ -1,10 +1,14 @@
 import shutil, os
 
+# Do not run the standard tests on these test problems.
+ignore_list = ('GravityTest',)
+
 template = open("test_type.py.template").read()
 
 for root, dirs, files in os.walk("."):
     for fn in files:
-        if fn.endswith(".enzo"):
+        if fn.endswith(".enzo") and \
+          os.path.basename(fn)[:-5] not in ignore_list:
             simname = os.path.splitext(fn)[0]
             simpath = root
             oname = os.path.join(root, "test_standard.py")
