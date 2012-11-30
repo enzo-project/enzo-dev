be1ae43aba98b6893c204bedfb29bfc6b0b02e32
Nathan Goldbaum <goldbaum@ucolick.org>
2012-11-30 14:19:12 -0800

    Adding an almost_standard test for Toro-6.
    --HG--
    branch : week-of-code

--- run/Hydro/Hydro-1D/Toro-6-ShockTube/Toro-6-ShockTube__test_toro6.py
+++ run/Hydro/Hydro-1D/Toro-6-ShockTube/Toro-6-ShockTube__test_toro6.py
@@ -20,3 +20,23 @@ def test_toro6():
     test = ShockTubeTest(_data_file, _solution_file, _fields, 
                          _les, _res, _rtol, _atol)
     return test()
+
+_base_fields = ('Density', 'TotalEnergy')
+
+def test_almost_standard():
+    sim = sim_dir_load("Toro-6-ShockTube.enzo",
+                       path="./Hydro/Hydro-1D/Toro-6-ShockTube",
+                       find_outputs=True)
+    sim.get_time_series()
+    yield VerifySimulationSameTest(sim)
+    base_pf = sim[0]
+    fields = [f for f in _base_fields if f in base_pf.h.field_list]
+    # Only test the last output.
+    pf = sim[-1]
+    for test in standard_small_simulation(pf, fields): yield test
+
+# Tests that OutputLog exists and fails otherwise
+def test_exist():
+    filename = os.path.dirname(__file__) + "/OutputLog"
+    if not os.path.exists(filename):
+        raise EnzoTestOutputFileNonExistent(filename)
