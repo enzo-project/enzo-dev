010d51555ce112b64ad7c665b261c08fe8895833
David Collins (dcollins4096@gmail.com) <David Collins (dcollins4096@gmail.com)>
2013-01-18 09:09:04 -0700

    Fixing a glitch wherein acceleration field is not zeroed if external gravity is on, self gravity is not, and acceleration boundary is on
    --HG--
    branch : week-of-code

--- src/enzo/Grid_ComputeAccelerationFieldExternal.C
+++ src/enzo/Grid_ComputeAccelerationFieldExternal.C
@@ -66,8 +66,12 @@ int grid::ComputeAccelerationFieldExternal()
     for (dim = 0; dim < GridRank; dim++) {
       AccelerationField[dim] = new float[size];
       for (i = 0; i < size; i++)
-	AccelerationField[dim][i] = 0;
+        AccelerationField[dim][i] = 0;
     }
+  if( SelfGravity == 0 )
+      for (i = 0; i < size; i++)
+        AccelerationField[dim][i] = 0;
+
  
 
   /* BWO:  if we're using the NFW halo with ProblemType = 31 ("GalaxySimulation"),
