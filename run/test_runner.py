f64b18972d8789e6743ab7ee6b6b776e9c81d5e5
David Collins (dcollins4096@gmail.com) <David Collins (dcollins4096@gmail.com)>
2013-01-22 16:11:56 -0700

    Adding MHDCT Orszag to the ignore_list.  Also adding file existance check to removal
    --HG--
    branch : week-of-code

--- run/test_runner.py
+++ run/test_runner.py
@@ -742,7 +742,7 @@ if __name__ == "__main__":
     # Same with MHD2DRotorTest
     ignore_list = ('GravityTest', 'ProtostellarCollapse_Std',
                    'ZeldovichPancake', 'AMRZeldovichPancake',
-                   'MHD2DRotorTest', 'Toro-6-ShockTube')
+                   'MHD2DRotorTest', 'Toro-6-ShockTube', 'MHDCTOrszagTangAMR', 'MHDCTOrszagTang')
     
     template = open("test_type.py.template").read()
     
@@ -769,7 +769,8 @@ if __name__ == "__main__":
     # *__test_standard.py files from the enzo run/ directory
     # that we just created.
     for file in test_standard_files:
-        os.remove(file)
+        if os.path.exists(file):
+            os.remove(file)
 
     # Store the results locally or in the cloud.
     answer_plugin.finalize()
