a3b4bdf6884f59061b619178b508935d383c92be
bwoshea <oshea@msu.edu>
2013-03-27 13:33:44 -0400

    cleanup: removed stuff that's commented out.
    --HG--
    branch : week-of-code

--- src/enzo/Grid_MHD_Diagnose.C
+++ src/enzo/Grid_MHD_Diagnose.C
@@ -14,7 +14,6 @@ float PreviousEnergy;
 int grid::MHD_Diagnose(char * label)
 {
 
-
   if( MyProcessorNumber != ProcessorNumber )
     return SUCCESS;
 
@@ -61,11 +60,6 @@ int grid::MHD_Diagnose(char * label)
       return SUCCESS;
     }
 
-  /*  
-  FILE * eptr = fopen("file.Energies", "a");
-  FILE * dptr = fopen("file.Divergencies", "a");
-  */
-
   int DensNum, GENum, TENum, Vel1Num, Vel2Num, Vel3Num;
   
   if (this->IdentifyPhysicalQuantities(DensNum, GENum, Vel1Num, Vel2Num,
@@ -78,7 +72,6 @@ int grid::MHD_Diagnose(char * label)
   //Compute total energy, gas, kinetic, and magnetic energies
   //as well as divergence.
 
-  fprintf(stderr,"CLOWN here's the deal\n");
   for(k=GridStartIndex[2];k<=GridEndIndex[2]; k++)
     for(j=GridStartIndex[1];j<=GridEndIndex[1];j++)
       for(i=GridStartIndex[0];i<=GridEndIndex[0];i++){
@@ -101,22 +94,20 @@ int grid::MHD_Diagnose(char * label)
 	Mass += dens;
 	
 	GasEnergy += eng - 0.5*dens*(v1*v1+v2*v2+v3*v3) - 0.5*(b1*b1+b2*b2+b3*b3);
-	
 	  
-	  divergence = 
-	    (MagneticField[0][indexb1(i+1,j,k)] -MagneticField[0][indexb1(i,j,k)])/ (dx)+
-	    ( (GridRank < 2 ) ? 0 : 
-	      (MagneticField[1][indexb2(i,j+1,k)] -MagneticField[1][indexb2(i,j,k)])/ (dy )) +
-	    ( (GridRank < 3 ) ? 0 : 
-	      (MagneticField[2][indexb3(i,j,k+1)] -MagneticField[2][indexb3(i,j,k)])/ (dz )); 
+	divergence = 
+	  (MagneticField[0][indexb1(i+1,j,k)] -MagneticField[0][indexb1(i,j,k)])/ (dx)+
+	  ( (GridRank < 2 ) ? 0 : 
+	    (MagneticField[1][indexb2(i,j+1,k)] -MagneticField[1][indexb2(i,j,k)])/ (dy )) +
+	  ( (GridRank < 3 ) ? 0 : 
+	    (MagneticField[2][indexb3(i,j,k+1)] -MagneticField[2][indexb3(i,j,k)])/ (dz )); 
 	
 	if( fabs(divergence) > MaxDivB ) MaxDivB = fabs(divergence);
-    //fprintf(stderr,"CLOWN %0.2e %0.2e\n", divergence, MaxDivB);
+
 	if( divergence != divergence ) {
 	  fprintf(stderr,"Nan in divergence.  That's too bad...\n");
 	  if( Crash == TRUE )
 	    return FAIL;
-
 	}
 	DivB[index] = divergence;
 	
@@ -126,7 +117,6 @@ int grid::MHD_Diagnose(char * label)
 	// Keep track of the structure of the divergence, and one level of its geometry (only outer bounds).
 
 	if( fabs(divergence) > MaxTolerance ) {
-
 	  if( i > max[0] ) max[0] = i;
 	  if( i < min[0] ) min[0] = i;
 	  if( j > max[1] ) max[1] = j;
@@ -144,59 +134,23 @@ int grid::MHD_Diagnose(char * label)
 
   //Compute current
 
-
   if( MaxDivB >= MaxTolerance || TRUE ){
-  fprintf(stderr, " ++++ L1 norm             L0 norm           L1/GridSize        Max    %s\n",
-	  label);
-  fprintf(stderr, " ++++ %13.12e %13.12e %13.12e %13.12e\n", 
-	  AbsDivB, TotalDivB, AbsDivB/size, MaxDivB);
-  float IsThisNan = TotalDivB;
-  if( IsThisNan != IsThisNan ) {
-    fprintf(stderr,"000000000000000000000 Nan Tastic.  %13.12e \n", IsThisNan);
-    return FAIL;
-  }
-  //re-scale for idl units.  (Strip off ghost zone.)
-  /*
-  for(i=0;i<3;i++){
-    min[i]-=3;
-    max[i]-=3;
-  }
-  */
-
-  
-  float LeftEdgeCell[3], RightEdgeCell[3];
-  for(int mung=0;mung<3;mung++){
-    //this shit doesn't work. Curently this shit is hard coded. Sorry
-    //LeftEdgeCell[mung]=GridLeftEdge[mung]*(GridDimension[mung]-6);
-    //RightEdgeCell[mung]=GridRightEdge[mung]*(GridDimension[mung]-6)-1;
-    LeftEdgeCell[mung]=GridLeftEdge[mung]*16;
-    RightEdgeCell[mung]=GridRightEdge[mung]*16-1;
-  }
-  
-  fprintf( stderr, " ++++ [%d:%d, %d:%d, %d:%d] Convex = %d"
-	   "Dims: %d %d %d Left RG cell: %f %f %f\n",
-
-	   min[0], max[0], min[1], max[1], min[2], max[2], Convex
-	   , GridDimension[0], GridDimension[1], GridDimension[2]
-	   , LeftEdgeCell[0], LeftEdgeCell[1], LeftEdgeCell[2]);
-  
-  char sayit[50];
-  sprintf(sayit, " divb : shit. %s", label);
-
+    fprintf(stderr, " ++++ L1 norm             L0 norm           L1/GridSize        Max    %s\n",
+	    label);
+    fprintf(stderr, " ++++ %13.12e %13.12e %13.12e %13.12e\n", 
+	    AbsDivB, TotalDivB, AbsDivB/size, MaxDivB);
+    float IsThisNan = TotalDivB;
+    if( IsThisNan != IsThisNan ) {
+      fprintf(stderr,"000000000000000000000 Nan Tastic.  %13.12e \n", IsThisNan);
+      return FAIL;
+    }
 
   if( Crash == TRUE )
     return FAIL;
   else
     return SUCCESS;
 
-
-
   }
-  //fprintf(dptr, " %13.12e %13.12e \n", AbsDivB, TotalDivB, AbsDivB/size);
-  //fclose(eptr);
-  //fclose(dptr);
-
-  //PreviousEnergy = TotalEnergy;
 
   return SUCCESS;
 
