8904bd3c9ad4ecdc72c130662fdc47b40024c904
Cameron Hummels <chummels@astro.columbia.edu>
2013-11-12 13:41:29 -0700

    Applying corrections suggested by Sam: removing all tabs in indentation, resetting indentation, splitting shear calculation into two separate loops.
    --HG--
    branch : week-of-code

--- src/enzo/Grid_FlagCellsToBeRefinedByShear.C
+++ src/enzo/Grid_FlagCellsToBeRefinedByShear.C
@@ -27,15 +27,15 @@
 #include "phys_constants.h"
 
 int GetUnits(float *DensityUnits, float *LengthUnits,
-         float *TemperatureUnits, float *TimeUnits,
-         float *VelocityUnits, FLOAT Time);
+             float *TemperatureUnits, float *TimeUnits,
+             float *VelocityUnits, FLOAT Time);
  
 int grid::FlagCellsToBeRefinedByShear()
 {
   /* declarations */
  
   int i, j, k, index, dim, LeftOffset, RightOffset;
-  float Shear2, Denom, Norm2;
+  float Shear2, Denom, c_sound2;
  
   /* error check */
  
@@ -52,7 +52,7 @@ int grid::FlagCellsToBeRefinedByShear()
  
   /* Allocate temp arrays and fill with zeroes */
  
-  float *DelVel1= new float[size];
+  float *DelVel1 = new float[size];
   float *DelVel2 = new float[size];
   float *DelVel3 = new float[size];
 
@@ -65,13 +65,12 @@ int grid::FlagCellsToBeRefinedByShear()
   /* Find fields: density, total energy, velocity1-3. */
  
   int DensNum, GENum, TENum, Vel1Num, Vel2Num, Vel3Num;
-  float c_sound2;
   if (this->IdentifyPhysicalQuantities(DensNum, GENum, Vel1Num, Vel2Num,
-				       Vel3Num, TENum) == FAIL) {
+                                       Vel3Num, TENum) == FAIL) {
     ENZO_FAIL("Error in IdentifyPhysicalQuantities.\n");
   }
 
-/* Create pressure field for calculating local sound speed */
+  /* Create pressure field for calculating local sound speed */
 
   float *pressure = new float[size];
 
@@ -80,10 +79,10 @@ int grid::FlagCellsToBeRefinedByShear()
   }
 
   float TemperatureUnits = 1, DensityUnits = 1, LengthUnits = 1,
-    VelocityUnits = 1, TimeUnits = 1;
+        VelocityUnits = 1, TimeUnits = 1;
 
   if (GetUnits(&DensityUnits, &LengthUnits, &TemperatureUnits,
-           &TimeUnits, &VelocityUnits, Time) == FAIL) {
+               &TimeUnits, &VelocityUnits, Time) == FAIL) {
     ENZO_FAIL("Error in GetUnits.");
   }
 
@@ -102,7 +101,7 @@ int grid::FlagCellsToBeRefinedByShear()
 
   /* The old method for this refinement criterion refined on both
      shear and vorticity, and it made a number of assumptions about
-     the simulation (c_s=1; PPM; etc.), but I leave it below 
+     the simulation (c_s=1; PPM; etc.), but it is left below 
      for reproducibility with old results.  To utilize the old method
      for shear refinement, include the parameter "OldShearMethod = 1" in your
      parameter file.
@@ -113,72 +112,72 @@ int grid::FlagCellsToBeRefinedByShear()
   
   /* loop over active dimensions */
  
-  for (dim = 0; dim < GridRank; dim++)
-    if (GridDimension[dim] > 1) {
+    for (dim = 0; dim < GridRank; dim++) {
+      if (GridDimension[dim] > 1) {
  
-      /* Absolute Shear: [(dvx/dy + dvy/dx)^2 + (dvz/dy + dvy/dz)^2 + 
-                          (dvx/dz + dvz/dx)^2  ]^(0.5)
-	     where:  dvx/dy = [vx(j-1) - vx(j+1)]/[2dy],
-         in units of s^(-1)
+  /* Absolute Shear: [(dvx/dy + dvy/dx)^2 + (dvz/dy + dvy/dz)^2 + 
+                      (dvx/dz + dvz/dx)^2  ]^(0.5)
+     where:  dvx/dy = [vx(j-1) - vx(j+1)]/[2dy],
+     in units of s^(-1)
     
-         In order to use this as a refinement criterion, we need to compare
-         it against the local cell width (ie dx).  So we refine when
-         the shearing rate is larger than a user-defined threshold
-         divided by the sound-crossing time of the cell:
+     In order to use this as a refinement criterion, we need to compare
+     it against the local cell width (ie dx).  So we refine when
+     the shearing rate is larger than a user-defined threshold
+     divided by the sound-crossing time of the cell:
 
-         Shear > epsilon * c_s / dx
+     Shear > epsilon * c_s / dx
 
-         We can save some extra calculations by removing dx from 
-         the denominators of both sides of this equation:
+     We can save some extra calculations by removing dx from 
+     the denominators of both sides of this equation:
 
-         ((dvx + dvy)^2 + (dvz + dvy)^2 + (dvx + dvz)^2) / c_sound^2 > epsilon^2
+     ((dvx + dvy)^2 + (dvz + dvy)^2 + (dvx + dvz)^2) / c_sound^2 > epsilon^2
 
-      N.B.: DelVel1 = dvx(in dy) + dvy(in dx)
-            DelVel2 = dvx(in dz) + dvz(in dx)
-            DelVel3 = dvz(in dy) + dvy(in dz)
-     */
+     N.B.: DelVel1 = dvx(in dy) + dvy(in dx)
+           DelVel2 = dvx(in dz) + dvz(in dx)
+           DelVel3 = dvz(in dy) + dvy(in dz)
+  */
  
-      for (k = GridStartIndex[2]; k <= GridEndIndex[2]; k++)
-	  for (j = GridStartIndex[1]; j <= GridEndIndex[1]; j++)
-	  for (i = GridStartIndex[0]; i <= GridEndIndex[0]; i++) {
- 
-	    index = i + j*GridDimension[0] +
-	                k*GridDimension[1]*GridDimension[0];
+        for (k = GridStartIndex[2]; k <= GridEndIndex[2]; k++)
+          for (j = GridStartIndex[1]; j <= GridEndIndex[1]; j++)
+            for (i = GridStartIndex[0]; i <= GridEndIndex[0]; i++) {
+              index = i + j*GridDimension[0] +
+                      k*GridDimension[1]*GridDimension[0];
 
-     // CellWidth[dim][k] is used because k always represents the
-     // current cellwidth (i.e. dx, dy, dz), whereas j and i seem
-     // to get strange behavior, so it is tacitly assumed dx=dy=dz
+              switch (dim) {
+                case 0: 
+                  if (GridRank > 1) // dvy (in dx)
+                    DelVel1[index] += (BaryonField[Vel2Num][index - LeftOffset] -
+                                       BaryonField[Vel2Num][index + RightOffset]);
+                  if (GridRank > 2) // dvz (in dx)
+                    DelVel2[index] += (BaryonField[Vel3Num][index - LeftOffset] -
+                                       BaryonField[Vel3Num][index + RightOffset]);
+                  break;
  
-	    switch (dim) {
-	    case 0: 
-	      if (GridRank > 1) // dvy/dx
-          DelVel1[index] += (BaryonField[Vel2Num][index - LeftOffset] -
-                             BaryonField[Vel2Num][index + RightOffset]);
-	      if (GridRank > 2) // dvz/dx
-          DelVel2[index] += (BaryonField[Vel3Num][index - LeftOffset] -
-                             BaryonField[Vel3Num][index + RightOffset]);
-	      break;
+                case 1:             // dvx (in dy)
+                  DelVel1[index] += (BaryonField[Vel1Num][index - LeftOffset] -
+                                     BaryonField[Vel1Num][index + RightOffset]);
+                  if (GridRank > 2) // dvz (in dy)
+                    DelVel3[index] += (BaryonField[Vel3Num][index - LeftOffset] -
+                                       BaryonField[Vel3Num][index + RightOffset]);
+                  break;
  
-	    case 1:             // dvx/dy
-	      DelVel1[index] += (BaryonField[Vel1Num][index - LeftOffset] -
-	   	                     BaryonField[Vel1Num][index + RightOffset]);
-	      if (GridRank > 2) // dvz/dy
-		  DelVel3[index] += (BaryonField[Vel3Num][index - LeftOffset] -
-		                     BaryonField[Vel3Num][index + RightOffset]);
-	      break;
+                case 2:             // dvx (in dz) and dvy (in dz)
+                  DelVel2[index] += (BaryonField[Vel1Num][index - LeftOffset] -
+                                     BaryonField[Vel1Num][index + RightOffset]);
+                  DelVel3[index] += (BaryonField[Vel2Num][index - LeftOffset] -
+                                     BaryonField[Vel2Num][index + RightOffset]);
+                  break;
  
-	    case 2:             // dvx/dz and dvy/dz 
-	      DelVel2[index] += (BaryonField[Vel1Num][index - LeftOffset] -
-		                     BaryonField[Vel1Num][index + RightOffset]);
-		  DelVel3[index] += (BaryonField[Vel2Num][index - LeftOffset] -
-		                     BaryonField[Vel2Num][index + RightOffset]);
-	      break;
- 
-	    default:
-	      break;
-	    }
- 
-	    if (dim == GridRank-1) {    // if we're done
+                default:
+                  break;
+	          }
+            }
+        LeftOffset *= GridDimension[dim];
+        RightOffset *= GridDimension[dim];
+      }  // end loop over dimension
+
+  /* Combine all finite differences to determine if criterion is met */
+      for (index = 0; index < size; index++) {
 
         // If ideal gas, then calculate local sound speed
         if (EOSType == 0) 
@@ -195,91 +194,82 @@ int grid::FlagCellsToBeRefinedByShear()
 
         FlaggingField[index] +=
         (Shear2 > (MinimumShearForRefinement*MinimumShearForRefinement)) ? 1 : 0;
-        }
-	  }
- 
-      LeftOffset *= GridDimension[dim];
-      RightOffset *= GridDimension[dim];
- 
-    }  // end loop over dimension
+      }
+    }
+  } else {  /* OldShearMethod -- the old method for calculating shear 
+                                 (which includes vorticity)
+            */
+    int Offset = LeftOffset;
 
-  } else {  // OldShearMethod -- the old method for calculating shear 
-            // (and vorticity)
+    for (dim = 0; dim < GridRank; dim++) {
+      if (GridDimension[dim] > 1) {
 
-  int Offset = LeftOffset;
+  /* For shear: [(du/dy)^2 + (dv/dx)^2 + (dw/dy)^2 +
+                 (du/dz)^2 + (dv/dz)^2 + (dw/dx)^2  ] > parameter,
+     where:  du/dy = [u(j-1) - u(j+1)]/[2dy],
+     assume: dx=dy=dz;
+     parameter ~ (sound/dx)^2
+     assume: sound = 1
+  */
 
-  for (dim = 0; dim < GridRank; dim++)
-    if (GridDimension[dim] > 1) {
+        for (k = GridStartIndex[2]; k <= GridEndIndex[2]; k++)
+          for (j = GridStartIndex[1]; j <= GridEndIndex[1]; j++)
+            for (i = GridStartIndex[0]; i <= GridEndIndex[0]; i++) {
 
-      /* 
-         For shear: [(du/dy)^2 + (dv/dx)^2 + (dw/dy)^2 +
-                     (du/dz)^2 + (dv/dz)^2 + (dw/dx)^2  ] > parameter,
-         where:  du/dy = [u(j-1) - u(j+1)]/[2dy],
-         assume: dx=dy=dz;
-         parameter ~ (sound/dx)^2
-         assume: sound = 1
-      */
+              index = i + j*GridDimension[0] +
+                      k*GridDimension[1]*GridDimension[0];
 
-      for (k = GridStartIndex[2]; k <= GridEndIndex[2]; k++)
-        for (j = GridStartIndex[1]; j <= GridEndIndex[1]; j++)
-          for (i = GridStartIndex[0]; i <= GridEndIndex[0]; i++) {
+              float DelVel1 = 0.0, DelVel2 = 0.0;
+              switch (dim) {
+                case 0:
+                  if (GridRank > 1)
+                    DelVel1 = BaryonField[Vel1Num+1][index - Offset] -
+                              BaryonField[Vel1Num+1][index + Offset];
+                  if (GridRank > 2)
+                    DelVel2 = BaryonField[Vel1Num+2][index - Offset] -
+                              BaryonField[Vel1Num+2][index + Offset];
+                  break;
 
-            index = i + j*GridDimension[0] +
-                        k*GridDimension[1]*GridDimension[0];
+                case 1:
+                  DelVel1 = BaryonField[Vel1Num][index - Offset] -
+                            BaryonField[Vel1Num][index + Offset];
+                  if (GridRank > 2)
+                    DelVel2 = BaryonField[Vel1Num+2][index - Offset] -
+                              BaryonField[Vel1Num+2][index + Offset];
+                  break;
 
-            float DelVel1 = 0.0, DelVel2 = 0.0;
-            switch (dim) {
-            case 0:
-              if (GridRank > 1)
-                DelVel1 = BaryonField[Vel1Num+1][index - Offset] -
-                          BaryonField[Vel1Num+1][index + Offset];
-              if (GridRank > 2)
-                DelVel2 = BaryonField[Vel1Num+2][index - Offset] -
-                          BaryonField[Vel1Num+2][index + Offset];
-              break;
+                case 2:
+                  if (GridRank > 1)
+                    DelVel1 = BaryonField[Vel1Num+1][index - Offset] -
+                              BaryonField[Vel1Num+1][index + Offset];
+                    DelVel2 = BaryonField[Vel1Num][index - Offset] -
+                              BaryonField[Vel1Num][index + Offset];
+                  break;
 
-            case 1:
-              DelVel1 = BaryonField[Vel1Num][index - Offset] -
-                        BaryonField[Vel1Num][index + Offset];
-              if (GridRank > 2)
-                DelVel2 = BaryonField[Vel1Num+2][index - Offset] -
-                          BaryonField[Vel1Num+2][index + Offset];
-              break;
+                default:
+                  break;
+              }
 
-            case 2:
-              if (GridRank > 1)
-                DelVel1 = BaryonField[Vel1Num+1][index - Offset] -
-                          BaryonField[Vel1Num+1][index + Offset];
-              DelVel2 = BaryonField[Vel1Num][index - Offset] -
-                        BaryonField[Vel1Num][index + Offset];
-              break;
-
-            default:
-              break;
+              DelVel1 *= DelVel1;
+              DelVel2 *= DelVel2;
+              DelVel3[index] += DelVel1 + DelVel2;
+              if (dim == GridRank-1)
+                FlaggingField[index] +=
+                  (DelVel3[index] > MinimumShearForRefinement) ? 1 : 0;
             }
 
-            DelVel1 *= DelVel1;
-            DelVel2 *= DelVel2;
-            DelVel3[index] += DelVel1 + DelVel2;
-            if (dim == GridRank-1)
-              FlaggingField[index] +=
-                (DelVel3[index] > MinimumShearForRefinement) ? 1 : 0;
-          }
-
-      Offset *= GridDimension[dim];
-
-    }  // end loop over dimension
-
+        Offset *= GridDimension[dim];
+      }  // end loop over dimension
+    }
   }
+
   /* clean up */
- 
   delete [] DelVel1;
   delete [] DelVel2;
   delete [] DelVel3;
   delete [] pressure;
  
   /* Count number of flagged Cells. */
- 
   int NumberOfFlaggedCells = 0;
   for (i = 0; i < size; i++)
     if (FlaggingField[i])
@@ -287,5 +277,4 @@ int grid::FlagCellsToBeRefinedByShear()
       NumberOfFlaggedCells++;
  
   return NumberOfFlaggedCells;
- 
 }
