de4f493d539cf34e01f7e0cd2896eede875c36fe
John Wise <jwise@physics.gatech.edu>
2014-09-16 12:52:22 -0400

    Changing default for HierarchyFileOutputFormat to 1 (ASCII) instead of 2 (ASCII + HDF5).  The HDF5 format does not scale well and can take ~10-50x longer than the actual datadump for simulations with >100k grids.  We can set this back to the HDF5 format once the scaling issues are resolved.
    --HG--
    branch : week-of-code

--- src/enzo/SetDefaultGlobalValues.C
+++ src/enzo/SetDefaultGlobalValues.C
@@ -141,7 +141,7 @@ int SetDefaultGlobalValues(TopGridData &MetaData)
 
   // Default Hierarchy File IO settings (1 = ASCII; 2 = HDF5+ASCII)
   HierarchyFileInputFormat = 1;
-  HierarchyFileOutputFormat = 2;
+  HierarchyFileOutputFormat = 1;
 
   ConductionDynamicRebuildHierarchy = FALSE;
   ConductionDynamicRebuildMinLevel = 0;
