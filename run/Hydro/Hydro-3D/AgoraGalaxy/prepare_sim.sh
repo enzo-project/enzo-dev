40bce8fabb850fd4f845ac6ab57c8eb8178b2718
Nathan Goldbaum <goldbaum@ucolick.org>
2014-11-21 10:37:36 -0800

    Small tweak so ICs tarball always gets saved with the correct filename.
    --HG--
    branch : week-of-code

--- run/Hydro/Hydro-3D/AgoraGalaxy/prepare_sim.sh
+++ run/Hydro/Hydro-3D/AgoraGalaxy/prepare_sim.sh
@@ -1,4 +1,4 @@
-wget https://www.dropbox.com/sh/1xzt1rysy9v3a9l/AAAMlJBQG1OQFW4cjhp11Ex6a/LOW.tar.gz
+wget -O ./LOW.tar.gz https://www.dropbox.com/sh/1xzt1rysy9v3a9l/AAAMlJBQG1OQFW4cjhp11Ex6a/LOW.tar.gz?dl=1
 wget https://bitbucket.org/grackle/grackle/src/default/input/CloudyData_noUVB.h5
 tar xzvf LOW.tar.gz
 mv LOW/*.dat ./
