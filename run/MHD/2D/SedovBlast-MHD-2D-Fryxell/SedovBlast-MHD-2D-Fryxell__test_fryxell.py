733e68880507e1a0f31f8b5ea19eb06f6a952f9e
Nathan Goldbaum <ngoldbau@ucsc.edu>
2015-09-30 23:55:22 -0500

    Update test suite to work with yt-3
    --HG--
    branch : week-of-code

--- run/MHD/2D/SedovBlast-MHD-2D-Fryxell/SedovBlast-MHD-2D-Fryxell__test_fryxell.py
+++ run/MHD/2D/SedovBlast-MHD-2D-Fryxell/SedovBlast-MHD-2D-Fryxell__test_fryxell.py
@@ -15,11 +15,11 @@ class TestFryxellImage(AnswerTestingTest):
     _attrs = ("field", )
 
     def __init__(self, pf, field):
-        self.pf = pf
+        self.ds = pf
         self.field = field
 
     def run(self):
-        sl = self.pf.h.slice(2, 0.5)
+        sl = self.ds.slice(2, 0.5)
         frb = FixedResolutionBuffer(sl, (0.0, 1.0, 0.0, 1.0), (200,200))
         dd = frb[self.field]
         return np.array([dd.mean(), dd.std(), dd.min(), dd.max()])
