4a603eb2a80dfb52e4b672aa6f6db9bf6533866f
Tom Abel <tabel@stanford.edu>
2016-01-05 00:54:47 +0000

    Grid_SetNumberOfColours.C edited online with Bitbucket to add if (debug) statement.
    --HG--
    branch : week-of-code

--- src/enzo/hydro_rk/Grid_SetNumberOfColours.C
+++ src/enzo/hydro_rk/Grid_SetNumberOfColours.C
@@ -79,7 +79,8 @@ int grid::SetNumberOfColours(void)
   if (NSpecies != 0 && MultiSpecies == 0)
     NoMultiSpeciesButColors = 1;
 
-  fprintf(stdout, "grid:SetNumberOfColours: NEQ_HYDRO = %"ISYM", NSpecies = %"ISYM", NColor = %"ISYM"\n", 
+  if (debug) 
+    fprintf(stdout, "grid:SetNumberOfColours: NEQ_HYDRO = %"ISYM", NSpecies = %"ISYM", NColor = %"ISYM"\n", 
   	  NEQ_HYDRO, NSpecies, NColor); 
 
 
