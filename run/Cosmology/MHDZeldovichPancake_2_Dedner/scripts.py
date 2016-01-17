3397c6b68e9350f6f89da3ae1669e8e1c467daff
dcollins4096 <none@none>
2016-01-17 17:00:57 -0500

    Touching up plot scripts for MHD Zeldovich Pancakes
    --HG--
    branch : week-of-code

--- run/Cosmology/MHDZeldovichPancake_2_Dedner/scripts.py
+++ run/Cosmology/MHDZeldovichPancake_2_Dedner/scripts.py
@@ -5,18 +5,23 @@ from yt.mods import *
 pf = load('DD0001/data0001')
 ray = pf.h.ortho_ray(0,(0,0))
 
-plt.subplot(311)
+plt.subplot(411)
 plt.semilogy(ray['x'], ray['enzo','Density'], c='k', ls='-', marker='.', ms=5)
-plt.ylabel(r'Density (cm$^{-3}$)')
+plt.ylabel(r'Density')
 plt.xlim(0.42,0.58)
 
-plt.subplot(312)
+plt.subplot(412)
+plt.semilogy(ray['x'], ray['Temperature'], c='k', ls='-', marker='.', ms=5)
+plt.ylabel('Temperature')
+plt.xlim(0.42,0.58)
+
+plt.subplot(413)
 plt.plot(ray['x'], ray['x-velocity'], c='k', ls='-', marker='.', ms=5)
 plt.ylabel('Velocity')
 plt.xlabel('x')
 plt.xlim(0.42,0.58)
 
-plt.subplot(313)
+plt.subplot(414)
 plt.semilogy(ray['x'], ray['By'], c='k', ls='-', marker='.', ms=5)
 plt.ylabel('By')
 plt.xlabel('x')
