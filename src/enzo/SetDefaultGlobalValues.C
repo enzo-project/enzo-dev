3a3b3007b04dd82966430c730158b60e12ae9675
Andrew Emerick <emerick@astro.columbia.edu>
2016-05-10 11:53:03 -0400

    individual star supernova energy default modified to 1.0E51
    --HG--
    branch : week-of-code

--- src/enzo/SetDefaultGlobalValues.C
+++ src/enzo/SetDefaultGlobalValues.C
@@ -738,7 +738,7 @@ int SetDefaultGlobalValues(TopGridData &MetaData)
 
   IndividualStarFUVHeating           = 0;           // on or off - include Bakes & Tielens FUV Heating
 
-  IndividualStarSupernovaEnergy      = -1;          // when < 0, use factor x mc^2 for supernova energy injection
+  IndividualStarSupernovaEnergy      = 1;          // when < 0, use factor x mc^2 for supernova energy injection
                                                     // when > 0, constant supernova energy in units of 10^51 erg
   IndividualStarStellarWindVelocity  = -1;          // when < 0, use Leithener et. al. model for stellar wind velocities
                                                     // when > 0, uniform wind velocity for all stars in km / s
