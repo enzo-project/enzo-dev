c0265355db46077a14b978e5092230b4afd06e76
Andrew Emerick <emerick@astro.columbia.edu>
2016-06-16 14:19:44 -0400

    commented out unneeded pressure line
    --HG--
    branch : week-of-code

--- src/enzo/Grid_GalaxySimulationInitializeGrid.C
+++ src/enzo/Grid_GalaxySimulationInitializeGrid.C
@@ -995,8 +995,8 @@ float DiskPotentialCircularVelocity(FLOAT cellwidth, FLOAT z, FLOAT density,
 
 	bulgeComp = (DiskGravityStellarBulgeMass == 0.0 ?
 		     0.0 : qromb(PbulgeComp2, fabs(zicm2), fabs(z)));
-        Pressure2 = (bulgeComp + qromb(PstellarComp2, fabs(zicm2), fabs(z)))
-          *(0.5*(1.0+cos(pi*(r2-SmoothRadius*Mpc)/(SmoothLength*Mpc))));
+//        Pressure2 = (bulgeComp + qromb(PstellarComp2, fabs(zicm2), fabs(z)))
+//          *(0.5*(1.0+cos(pi*(r2-SmoothRadius*Mpc)/(SmoothLength*Mpc))));
 
 	Pressure2 = (bulgeComp + qromb(PDMComp2, fabs(zicm2), fabs(z)) + qromb(PstellarComp2, fabs(zicm2), fabs(z)))
 	  *(0.5*(1.0+cos(pi*(r2-SmoothRadius*Mpc)/(SmoothLength*Mpc))));
