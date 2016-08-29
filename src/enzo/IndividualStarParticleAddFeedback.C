111eee30365e7be75ddb49a6e27d64b2541d0620
Andrew Emerick <emerick@astro.columbia.edu>
2016-08-29 13:46:01 -0400

    bugfix in updating mass after feedback - mass loss was being accumulated many times if feedback occured across grids. Bad
    --HG--
    branch : week-of-code

--- src/enzo/IndividualStarParticleAddFeedback.C
+++ src/enzo/IndividualStarParticleAddFeedback.C
@@ -86,6 +86,12 @@ int IndividualStarParticleAddFeedback(TopGridData *MetaData,
       continue; // only apply feedback on level of star
     }
 
+    if(cstar->ReturnMass() < 0.0){
+        cstar->PrintInfo();
+        ENZO_FAIL("Particle Mass initially negative in IndividualStarParticleAddFeedback");
+    }
+
+
     /* feedback is done in a cic interpolation. This is number of cells
        on eiher side of central cell (i.e. 3x3 CIC -> ncell = 1) */
     int ncell = (int) ((IndividualStarFeedbackStencilSize+1)/2.0 - 1);
@@ -93,7 +99,7 @@ int IndividualStarParticleAddFeedback(TopGridData *MetaData,
     pos = cstar->ReturnPosition();
     vel = cstar->ReturnVelocity();
 
-    double particle_mass = cstar->ReturnMass();
+    double particle_mass;
 
     //
     // Check Stellar Winds - Apply if particle on grid and grid local
@@ -105,6 +111,9 @@ int IndividualStarParticleAddFeedback(TopGridData *MetaData,
         for (Temp = LevelArray[l]; Temp; Temp = Temp ->NextGridThisLevel){
 
           if(Temp->GridData->isLocal() && IsParticleFeedbackInGrid(pos, ncell, Temp) ){
+            // refresh mass every time to prevent double+ counting loss
+            particle_mass = cstar->ReturnMass();
+
             Temp->GridData->IndividualStarAddFeedbackGeneral(pos[0], pos[1], pos[2],
                                                            vel[0], vel[1], vel[2],
                                                            cstar->ReturnBirthMass(), cstar->ReturnLifetime(),
@@ -112,16 +121,15 @@ int IndividualStarParticleAddFeedback(TopGridData *MetaData,
                                                            cstar->ReturnMetallicity(), &particle_mass, -1);
                                                            // < 0 in last arg signifies stellar winds
 
-            cstar->SetNewMass(particle_mass); // CHECK MASS UNITS
             AddedFeedback[count] = true;
           }
         }
       }
+      cstar->SetNewMass(particle_mass); // update mass (only once)
     }
 
     //
     // Check Core Collapse Supernova
-    //
     if( cstar->ReturnFeedbackFlag() == INDIVIDUAL_STAR_SNII ||
         cstar->ReturnFeedbackFlag() == INDIVIDUAL_STAR_WIND_AND_SN){
       for (int l = level; l < MAX_DEPTH_OF_HIERARCHY; l ++){
@@ -129,6 +137,7 @@ int IndividualStarParticleAddFeedback(TopGridData *MetaData,
         for (Temp = LevelArray[l]; Temp; Temp = Temp ->NextGridThisLevel){
 
           if(Temp->GridData->isLocal() && IsParticleFeedbackInGrid(pos, ncell, Temp)){
+            // refresh mass every time to prevent double+ counting
             particle_mass = cstar->ReturnMass();
 
             Temp->GridData->IndividualStarAddFeedbackGeneral(pos[0], pos[1], pos[2],
@@ -138,11 +147,12 @@ int IndividualStarParticleAddFeedback(TopGridData *MetaData,
                                                            cstar->ReturnMetallicity(), &particle_mass, 1);
                                                            // 1 in last arg signifies Core collapse SN
 
-            cstar->SetNewMass(particle_mass); // CHECK MASS UNITS
             AddedFeedback[count] = true;
           }
         }
       }
+
+      cstar->SetNewMass(particle_mass); // update mass (only once)
     }
 
     //
@@ -153,6 +163,7 @@ int IndividualStarParticleAddFeedback(TopGridData *MetaData,
         for (Temp = LevelArray[l]; Temp; Temp = Temp ->NextGridThisLevel){
 
           if(Temp->GridData->isLocal() && IsParticleFeedbackInGrid(pos, ncell, Temp)){
+            particle_mass = cstar->ReturnMass();
 
             Temp->GridData->IndividualStarAddFeedbackGeneral(pos[0], pos[1], pos[2],
                                                            vel[0], vel[1], vel[2],
@@ -161,11 +172,17 @@ int IndividualStarParticleAddFeedback(TopGridData *MetaData,
                                                            cstar->ReturnMetallicity(), &particle_mass, 2);
                                                            // 2 in last arg signifies Type 1a
 
-            cstar->SetNewMass( 0.0 ); // masless tracer after explosion
             AddedFeedback[count] = true;
           }
         }
       }
+
+      cstar->SetNewMass(0.0); // now a massless tracer
+    }
+
+    if(cstar->ReturnMass() < 0.0){
+        cstar->PrintInfo();
+        ENZO_FAIL("Particle Mass going negative in IndividualStarParticleAddFeedback");
     }
 
   } // end stars loop
