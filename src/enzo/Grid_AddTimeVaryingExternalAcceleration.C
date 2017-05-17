63bf9cb207f6ba7b2ae93910d57dfb66ab721190
Andrew Emerick <emerick@astro.columbia.edu>
2017-05-17 16:45:27 -0400

    mpc to kpc for position input
    --HG--
    branch : week-of-code

--- src/enzo/Grid_AddTimeVaryingExternalAcceleration.C
+++ src/enzo/Grid_AddTimeVaryingExternalAcceleration.C
@@ -69,11 +69,11 @@ int grid::AddTimeVaryingExternalAcceleration(void){
   // find coordinates and normalize to box frame coordinates, which is what grid cell and particle positions will be in
   // ExternalGravityPosition is initally in coordinates relative to the center of the secondary galaxy
   ExternalGravityPosition[0] = ((1.0 - coeff)*ExternalGravityTimePositions[0][index] +
-                               (      coeff)*ExternalGravityTimePositions[0][index+1])*Mpc/LengthUnits + DiskGravityPosition[0];
+                               (      coeff)*ExternalGravityTimePositions[0][index+1])*kpc/LengthUnits + DiskGravityPosition[0];
   ExternalGravityPosition[1] = ((1.0 - coeff)*ExternalGravityTimePositions[1][index] +
-                               (      coeff)*ExternalGravityTimePositions[1][index+1])*Mpc/LengthUnits + DiskGravityPosition[1];
+                               (      coeff)*ExternalGravityTimePositions[1][index+1])*kpc/LengthUnits + DiskGravityPosition[1];
   ExternalGravityPosition[2] = ((1.0 - coeff)*ExternalGravityTimePositions[2][index] +
-                               (      coeff)*ExternalGravityTimePositions[2][index+1])*Mpc/LengthUnits + DiskGravityPosition[2];
+                               (      coeff)*ExternalGravityTimePositions[2][index+1])*kpc/LengthUnits + DiskGravityPosition[2];
 
 
   FLOAT x, y, z, xpos, ypos, zpos, r, rsquared;
