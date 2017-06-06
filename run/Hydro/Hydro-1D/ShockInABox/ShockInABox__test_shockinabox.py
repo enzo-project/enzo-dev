25cbcf349a1a97334d32810e3c623c6b8e19c2de
bwoshea <oshea@msu.edu>
2017-06-06 12:42:48 -0400

    un-deleted ShockInABox test
    --HG--
    branch : week-of-code

---
+++ run/Hydro/Hydro-1D/ShockInABox/ShockInABox__test_shockinabox.py
@@ -0,0 +1,16 @@
+from yt.mods import *
+from yt.funcs import *
+from yt.testing import *
+from yt.frontends.enzo.answer_testing_support import \
+    requires_outputlog
+from yt.utilities.answer_testing.framework import AssertWrapper
+import os
+
+# Verifies that OutputLog exists
+@requires_outputlog(os.path.dirname(__file__), "ShockInABox.enzo")
+def test_shockinabox():
+    pf = load('DD0010/data0010')
+    mach = pf.h.find_max('Mach')[0]
+    myname = 'ShockInABox_Mach'
+    yield AssertWrapper(myname, assert_allclose, mach, 3.0, 1.0e-2)
+
