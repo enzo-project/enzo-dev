cd9de024f35aacd56e0f7192310116012d388539
Britton Smith <brittonsmith@gmail.com>
2018-01-30 10:03:50 -0800

    Temporarily commenting out all cosmology calculation formulae other than the tabular method.
    --HG--
    branch : week-of-code

--- src/enzo/CosmologyComputeExpansionFactor.C
+++ src/enzo/CosmologyComputeExpansionFactor.C
@@ -60,84 +60,90 @@ int CosmologyComputeExpansionFactor(FLOAT time, FLOAT *a, FLOAT *dadt)
  
   FLOAT TimeHubble0 = time * TimeUnits * (HubbleConstantNow*3.24e-18);
  
-  /* 1) For a flat universe with OmegaMatterNow = 1, it's easy. */
+//   /* 1) For a flat universe with OmegaMatterNow = 1, it's easy. */
  
-  if (fabs(OmegaMatterNow-1) < OMEGA_TOLERANCE &&
-      OmegaLambdaNow < OMEGA_TOLERANCE)
-    *a      = POW(time/InitialTimeInCodeUnits, FLOAT(2.0/3.0));
+//   if (fabs(OmegaMatterNow-1) < OMEGA_TOLERANCE &&
+//       OmegaLambdaNow < OMEGA_TOLERANCE)
+//     *a      = POW(time/InitialTimeInCodeUnits, FLOAT(2.0/3.0));
  
-#define INVERSE_HYPERBOLIC_EXISTS
+// #define INVERSE_HYPERBOLIC_EXISTS
  
-#ifdef INVERSE_HYPERBOLIC_EXISTS
+// #ifdef INVERSE_HYPERBOLIC_EXISTS
  
-  FLOAT eta, eta_old, x;
-  int i;
+//   FLOAT eta, eta_old, x;
+//   int i;
  
-  /* 2) For OmegaMatterNow < 1 and OmegaLambdaNow == 0 see
-        Peebles 1993, eq. 13-3, 13-10.
-	Actually, this is a little tricky since we must solve an equation
-	of the form eta - sinh(eta) + x = 0..*/
+//   /* 2) For OmegaMatterNow < 1 and OmegaLambdaNow == 0 see
+//         Peebles 1993, eq. 13-3, 13-10.
+// 	Actually, this is a little tricky since we must solve an equation
+// 	of the form eta - sinh(eta) + x = 0..*/
  
-  if (OmegaMatterNow < 1 && OmegaLambdaNow < OMEGA_TOLERANCE) {
+//   if (OmegaMatterNow < 1 && OmegaLambdaNow < OMEGA_TOLERANCE) {
  
-    x = 2*TimeHubble0*POW(1.0 - OmegaMatterNow, 1.5) / OmegaMatterNow;
+//     x = 2*TimeHubble0*POW(1.0 - OmegaMatterNow, 1.5) / OmegaMatterNow;
  
-    /* Compute eta in a three step process, first from a third-order
-       Taylor expansion of the formula above, then use that in a fifth-order
-       approximation.  Then finally, iterate on the formula itself, solving for
-       eta.  This works well because parts 1 & 2 are an excellent approximation
-       when x is small and part 3 converges quickly when x is large. */
+//     /* Compute eta in a three step process, first from a third-order
+//        Taylor expansion of the formula above, then use that in a fifth-order
+//        approximation.  Then finally, iterate on the formula itself, solving for
+//        eta.  This works well because parts 1 & 2 are an excellent approximation
+//        when x is small and part 3 converges quickly when x is large. */
  
-    eta = POW(6*x, FLOAT(1.0/3.0));                     // part 1
-    eta = POW(120*x/(20+eta*eta), FLOAT(1.0/3.0));      // part 2
-    for (i = 0; i < 40; i++) {                          // part 3
-      eta_old = eta;
-      eta = arcsinh(eta + x);
-      if (fabs(eta-eta_old) < ETA_TOLERANCE) break;
-    }
-    if (i == 40) {
-      ENZO_VFAIL("Case 2 -- no convergence after %"ISYM" iterations.\n", i)
-    }
+//     eta = POW(6*x, FLOAT(1.0/3.0));                     // part 1
+//     eta = POW(120*x/(20+eta*eta), FLOAT(1.0/3.0));      // part 2
+//     for (i = 0; i < 40; i++) {                          // part 3
+//       eta_old = eta;
+//       eta = arcsinh(eta + x);
+//       if (fabs(eta-eta_old) < ETA_TOLERANCE) break;
+//     }
+//     if (i == 40) {
+//       ENZO_VFAIL("Case 2 -- no convergence after %"ISYM" iterations.\n", i)
+//     }
  
-    /* Now use eta to compute the expansion factor (eq. 13-10, part 2). */
+//     /* Now use eta to compute the expansion factor (eq. 13-10, part 2). */
  
-    *a = OmegaMatterNow/(2*(1 - OmegaMatterNow))*(cosh(eta) - 1);
-    *a *= (1 + InitialRedshift);    // to convert to code units, divide by [a]
-  }
+//     *a = OmegaMatterNow/(2*(1 - OmegaMatterNow))*(cosh(eta) - 1);
+//     *a *= (1 + InitialRedshift);    // to convert to code units, divide by [a]
+//   }
  
-  /* 3) For OmegaMatterNow > 1 && OmegaLambdaNow == 0, use sin/cos.
-        Easy, but skip it for now. */
+//   /* 3) For OmegaMatterNow > 1 && OmegaLambdaNow == 0, use sin/cos.
+//         Easy, but skip it for now. */
  
-  if (OmegaMatterNow > 1 && OmegaLambdaNow < OMEGA_TOLERANCE) {
-  }
+//   if (OmegaMatterNow > 1 && OmegaLambdaNow < OMEGA_TOLERANCE) {
+//   }
  
-  /* 4) For flat universe, with non-zero OmegaLambdaNow, see eq. 13-20. */
+//   /* 4) For flat universe, with non-zero OmegaLambdaNow, see eq. 13-20. */
  
-  if (fabs(OmegaCurvatureNow) < OMEGA_TOLERANCE &&
-      OmegaLambdaNow > OMEGA_TOLERANCE) {
-    *a = POW(FLOAT(OmegaMatterNow/(1 - OmegaMatterNow)), FLOAT(1.0/3.0)) *
-         POW(FLOAT(sinh(1.5 * sqrt(1.0 - OmegaMatterNow)*TimeHubble0)),
-	     FLOAT(2.0/3.0));
-    *a *= (1 + InitialRedshift);    // to convert to code units, divide by [a]
-  }
+//   if (fabs(OmegaCurvatureNow) < OMEGA_TOLERANCE &&
+//       OmegaLambdaNow > OMEGA_TOLERANCE) {
+//     *a = POW(FLOAT(OmegaMatterNow/(1 - OmegaMatterNow)), FLOAT(1.0/3.0)) *
+//          POW(FLOAT(sinh(1.5 * sqrt(1.0 - OmegaMatterNow)*TimeHubble0)),
+// 	     FLOAT(2.0/3.0));
+//     *a *= (1 + InitialRedshift);    // to convert to code units, divide by [a]
+//   }
  
-#endif /* INVERSE_HYPERBOLIC_EXISTS */
+// #endif /* INVERSE_HYPERBOLIC_EXISTS */
 
-  /* 5) If we want to include the radiation term, use a table. */
+//   /* 5) If we want to include the radiation term, use a table. */
 
-  if (OmegaRadiationNow > 0.) {
-    if (CosmologyTableComputeExpansionFactor(TimeHubble0, a) == FAIL) {
+//   if (OmegaRadiationNow > 0.) {
+//     if (CosmologyTableComputeExpansionFactor(TimeHubble0, a) == FAIL) {
+//       ENZO_FAIL("Error in CosmologyTableComputeExpansionFactor.\n");
+//     }
+//     *a *= (1 + InitialRedshift);
+//   }
+
+//   /* Someday, we'll implement the general case... */
+
+//   if ((*a) == FLOAT_UNDEFINED) {
+//     ENZO_FAIL("Cosmology selected is not implemented.\n");
+
+//   }
+
+
+  if (CosmologyTableComputeExpansionFactor(TimeHubble0, a) == FAIL) {
       ENZO_FAIL("Error in CosmologyTableComputeExpansionFactor.\n");
-    }
-    *a *= (1 + InitialRedshift);
   }
-
-  /* Someday, we'll implement the general case... */
-
-  if ((*a) == FLOAT_UNDEFINED) {
-    ENZO_FAIL("Cosmology selected is not implemented.\n");
-
-  }
+  *a *= (1 + InitialRedshift);
  
   /* Compute the derivative of the expansion factor (Peebles93, eq. 13.3). */
  
