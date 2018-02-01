0098707174888270f7b19a237dfb23dca953c361
Britton Smith <brittonsmith@gmail.com>
2018-01-31 22:03:45 -0800

    Mandate numpy version.
    --HG--
    branch : week-of-code

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 wget --quiet https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
 bash ./Miniconda2-latest-Linux-x86_64.sh -b -p ./enzo-conda -f
 export PATH=$PWD/enzo-conda/bin:$PATH
-conda install -q -y mercurial cython h5py matplotlib sympy numpy pytest flake8 yt nose
+conda install -q -y mercurial cython h5py matplotlib sympy numpy=1.13.3 pytest flake8 yt nose
 pip install python-hglib
 
 # install OS dependencies
