c9cb4e21d5dec26d04429ba4c0cd439aeb499a6c
aemerick11@gmail.com <aemerick11@gmail.com>
2018-12-10 12:41:22 -0500

    Bugfix. Forgot to properly transfer stellar abundances data from grid-to-grid for agb,popIII,snia,snii fields
    --HG--
    branch : week-of-code

--- src/enzo/Grid_MoveParticleAbundances.C
+++ src/enzo/Grid_MoveParticleAbundances.C
@@ -34,12 +34,19 @@ int grid::MoveParticleAbundances(int NumberOfGrids, grid* FromGrid[]){
   this->AllocateStellarAbundances(NumberOfSubgridParticles);
 
   int Index = 0;
+  int offset = 0;
+
+  if (IndividualStarTrackAGBMetalDensity) offset++;  // AGB Metal tracer - ExtraField0
+  if (IndividualStarPopIIIFormation)      offset++;  // PopIII Metal Tracer - ExtraField1
+  if (IndividualStarTrackSNMetalDensity)  offset = offset + 2; // MetalSNIaDensity, MetalSNIIDensity
+
   for (grid = 0; grid < NumberOfGrids; grid++){
-    for (j = 0; j < StellarYieldsNumberOfSpecies; j++){
+    for (j = 0; j < StellarYieldsNumberOfSpecies + offset; j++){
       for (i = 0; i < FromGrid[grid]->NumberOfParticles; i++){
         StellarAbundances[j][Index+i] = FromGrid[grid]->StellarAbundances[j][i];
       }
     }
+
     Index += FromGrid[grid]->NumberOfParticles;
   }
 
