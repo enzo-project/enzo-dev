f8c69de41e9e44383de43b90c51d7ac93b600b3b
Andrew Emerick <aemerick11@gmail.com>
2019-06-11 15:17:39 -0700

    I'm dumb and forgot to commit the new key files for this PR

---
+++ src/enzo/Grid_PrepareBoundaryMassFluxFieldNumbers.C
@@ -0,0 +1,91 @@
+/***********************************************************************
+/
+/  GRID CLASS (INITIALIZE CONTAINERS FOR COUNTING DOMAIN BOUNDARY MASS FLUX)
+/
+/  written by: Andrew Emerick
+/  date:       2018-ish
+/
+/  PURPOSE:
+/           Sets up the container that keeps track of how much mass
+/           in each of the mass-density fields move across the external
+/           boundary
+/
+/  RETURNS: FAIL or SUCCESS
+/
+************************************************************************/
+
+#include <stdio.h>
+#include "ErrorExceptions.h"
+#include "macros_and_parameters.h"
+#include "typedefs.h"
+#include "global_data.h"
+#include "Fluxes.h"
+#include "GridList.h"
+#include "ExternalBoundary.h"
+#include "Grid.h"
+
+int FindField(int f, int farray[], int n);
+
+
+int grid::PrepareBoundaryMassFluxFieldNumbers(){
+
+  if (ProcessorNumber != MyProcessorNumber)
+    return SUCCESS;
+
+  if (!StoreDomainBoundaryMassFlux) return SUCCESS;
+
+  if (BoundaryMassFluxFieldNumbers[0] >= 0) return SUCCESS; // we've already initialized
+
+  /* obtain baryon field indexes */
+  int DensNum, GENum, TENum, Vel1Num, Vel2Num, Vel3Num, CRNum, B1Num, B2Num, B3Num;
+  this->DebugCheck("StarParticleHandler");
+  if (this->IdentifyPhysicalQuantities(DensNum, GENum, Vel1Num, Vel2Num,
+                                       Vel3Num, TENum, B1Num, B2Num, B3Num) == FAIL) {
+    ENZO_FAIL("Error in IdentifyPhysicalQuantities.");
+  }
+
+  /* identify species fields if they exist */
+  int DeNum, HINum, HIINum, HeINum, HeIINum, HeIIINum, HMNum, H2INum, H2IINum,
+      DINum, DIINum, HDINum;
+  if ( MultiSpecies ){
+    IdentifySpeciesFields(DeNum, HINum, HIINum, HeINum, HeIINum, HeIIINum,
+                          HMNum, H2INum, H2IINum, DINum, DIINum, HDINum);
+  }
+
+  /* get metallicity tracer field number */
+  int MetalNum;
+  MetalNum   = FindField(Metallicity, this->FieldType, this->NumberOfBaryonFields);
+
+  /* pre-compute field numbers so we only need to do this once */
+  int count = 0;
+  BoundaryMassFluxFieldNumbers[count++] = DensNum;
+
+  /* H, He and ionization states */
+  if (MultiSpecies > 0){
+    BoundaryMassFluxFieldNumbers[count++] = HINum;
+    BoundaryMassFluxFieldNumbers[count++] = HIINum;
+    BoundaryMassFluxFieldNumbers[count++] = HeINum;
+    BoundaryMassFluxFieldNumbers[count++] = HeIINum;
+    BoundaryMassFluxFieldNumbers[count++] = HeIIINum;
+  }
+
+  /* H-, H2, H2- */
+  if (MultiSpecies > 1){
+    BoundaryMassFluxFieldNumbers[count++] = HMNum;
+    BoundaryMassFluxFieldNumbers[count++] = H2INum;
+    BoundaryMassFluxFieldNumbers[count++] = H2IINum;
+  }
+
+  if (MultiSpecies > 2){
+    BoundaryMassFluxFieldNumbers[count++] = DINum;
+    BoundaryMassFluxFieldNumbers[count++] = DIINum;
+    BoundaryMassFluxFieldNumbers[count++] = HDINum;
+  }
+
+  /* metal density */
+  if (MultiMetals > 0){
+    BoundaryMassFluxFieldNumbers[count++] = MetalNum;
+  }
+
+  return SUCCESS;
+}
