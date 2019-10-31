5ee43795d33be4b92bf39f6638c03e17b717ec3a
Azton Wells <azton.wells@gmail.com>
2019-10-31 18:41:17 -0500

    Most recent updates from frontera

--- src/enzo/Grid_MechStarsDepositFeedback.C
+++ src/enzo/Grid_MechStarsDepositFeedback.C
@@ -199,7 +199,7 @@ int grid::MechStars_DepositFeedback(float ejectaEnergy,
     /* transform to comoving with the star and take velocities to momenta.
         Take Energy densities to energy.  winds are ngp unless VERY high resolution
      */
-    if (!winds || dx*LengthUnits/pc_cm < 0.1)
+    if (!winds || dx*LengthUnits/pc_cm < 2)
         transformComovingWithStar(BaryonField[DensNum], BaryonField[MetalNum], 
                         BaryonField[MetalIINum], BaryonField[MetalIaNum],
                         BaryonField[Vel1Num],BaryonField[Vel2Num],BaryonField[Vel3Num],
@@ -458,7 +458,7 @@ int grid::MechStars_DepositFeedback(float ejectaEnergy,
     As a computational compromize, supernova are deposited CIC, but winds are 
     deposited NGP.  At VERY high resolution, well still do CIC for winds
      */
-    if (!winds || dx*LengthUnits/pc_cm < 0.1){
+    if (!winds || dx*LengthUnits/pc_cm < 2){
         for (int n = 0; n < nCouple; ++n){
             pX = coupledMomenta*CloudParticleVectorX[n]*weightsVector[n];
             pY = coupledMomenta*CloudParticleVectorY[n]*weightsVector[n];
@@ -541,7 +541,7 @@ int grid::MechStars_DepositFeedback(float ejectaEnergy,
         the momentum goes uncoupled.  Since the cooling radius is so small for wind enrgy (~10^15 erg),
         this is totally appropriate for simulations with dx > 0.25pccm or so.
     */
-    if (winds && dx*LengthUnits/pc_cm > 0.1){
+    if (winds && dx*LengthUnits/pc_cm > 2){
         float dm = coupledMass/(density[index]+coupledMass);
         density[index] += coupledMass;
         metals[index] += coupledMetals;
@@ -587,7 +587,7 @@ int grid::MechStars_DepositFeedback(float ejectaEnergy,
     }
 
     /* Transform the grid back. Skip for winds, unless VERY high resolution*/
-    if (!winds || dx*LengthUnits/pc_cm < 0.1)
+    if (!winds || dx*LengthUnits/pc_cm < 2)
         transformComovingWithStar(BaryonField[DensNum], BaryonField[MetalNum], 
                         BaryonField[MetalIINum], BaryonField[MetalIaNum],
                         BaryonField[Vel1Num],BaryonField[Vel2Num],
