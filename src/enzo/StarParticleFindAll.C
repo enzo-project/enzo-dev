6148ab3273e0a6f355b8648ea6c53cd1d45f7b1d
Andrew Emerick <aemerick11@gmail.com>
2020-03-26 00:13:05 -0700

    Two Bugfixes from recent star particle memfix update

--- src/enzo/StarParticleFindAll.C
+++ src/enzo/StarParticleFindAll.C
@@ -153,7 +153,11 @@ int StarParticleFindAll(LevelHierarchyEntry *LevelArray[], Star *&AllStars)
         delete [] sendBuffer;
         sendBuffer = new StarBuffer[sendBufferSize];
       }
-      LocalStars->StarListToBuffer(sendBuffer, LocalNumberOfStars);
+
+      if (LocalNumberOfStars > 0)
+        LocalStars->StarListToBuffer(sendBuffer, LocalNumberOfStars);
+      else
+        sendBuffer = NULL;
 
       /* Share all data with all processors */
 
