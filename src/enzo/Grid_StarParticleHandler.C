a7f0ca67468036231a839e5aa8aa538777daa8c4
Andrew Emerick <aemerick11@gmail.com>
2020-04-13 17:35:30 -0700

    2nd Bugfix. Adding PopIII PISNe and RProcess field to density normalization at end of file

--- src/enzo/Grid_StarParticleHandler.C
+++ src/enzo/Grid_StarParticleHandler.C
@@ -2103,7 +2103,8 @@ int grid::StarParticleHandler(HierarchyEntry* SubgridPointer, int level,
  
   for (field = 0; field < NumberOfBaryonFields; field++) {
     if ((FieldType[field] >= ElectronDensity && FieldType[field] <= ExtraType1) ||
-	FieldType[field] == MetalSNIaDensity || FieldType[field] == MetalSNIIDensity) {
+	FieldType[field] == MetalSNIaDensity || FieldType[field] == MetalSNIIDensity || 
+        FieldType[field] == MetalRProcessDensity || FieldType[field] == MetalPISNeDensity){
 #ifdef EMISSIVITY
       /* 
          it used to be set to  FieldType[field] < GravPotential if Geoffrey's Emissivity0
