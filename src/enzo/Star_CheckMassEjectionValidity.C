385a16f80854f0077cebe3ca16a933400c864a32
Andrew Emerick <aemerick11@gmail.com>
2020-04-17 16:58:22 -0700

    be a little more lenient on mass ejection check"

--- src/enzo/Star_CheckMassEjectionValidity.C
+++ src/enzo/Star_CheckMassEjectionValidity.C
@@ -20,7 +20,7 @@ void Star::CheckMassEjectionValidity(void){
   if (abs(this->type) == PARTICLE_TYPE_INDIVIDUAL_STAR_UNRESOLVED)
     return;
 
-  float tolerance = 1.05; // 5 percent tolerance
+  float tolerance = 1.10; // 5 percent tolerance
   int sn_fail = 0, wind_fail = 0;
 
   float total_wind_mass_ejection = 0.0, total_sn_mass_ejection = 0.0;
