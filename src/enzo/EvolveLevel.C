5ccc9908669c2bb4fd32e068cce33e5e5f7bf8e8
Deovrat Deovrat <deovratd@dev-intel16.i>
2020-07-07 08:24:34 -0400

    removed merge conflicts

--- src/enzo/EvolveLevel.C
+++ src/enzo/EvolveLevel.C
@@ -742,21 +742,11 @@ int EvolveLevel(TopGridData *MetaData, LevelHierarchyEntry *LevelArray[],
 	Grids[grid1]->GridData->MagneticSupernovaList.clear(); 
     } //end loop over grids
 
-<<<<<<< HEAD
-    //ActiveParticleFinalize(Grids, MetaData, NumberOfGrids, LevelArray,
-    //                       level, NumberOfNewActiveParticles);
-    } //end loop over grids
-
     //Modification Added by Deovrat Prasad
-    // Finalize (accretion, feedback, etc. ) Active Particles.
-    ActiveParticleFinalize(Grids, MetaData, NumberOfGrids, LevelArray,
-                           level, NumberOfNewActiveParticles);
-
-=======
     /* Finalize (accretion, feedback etc) for Active particles. */
     ActiveParticleFinalize(Grids, MetaData, NumberOfGrids, LevelArray,
                            level, NumberOfNewActiveParticles);
->>>>>>> 815bd8137012ff783e3ec4109eedfd22a50d1c37
+
     /* Finalize (accretion, feedback, etc.) star particles */
     StarParticleFinalize(Grids, MetaData, NumberOfGrids, LevelArray,
 			 level, AllStars, TotalStarParticleCountPrevious, OutputNow);
