8b0cc08590c7f26263b86b2c607deadc57ebb79a
Simone Gordon <sgordon@cuillin.roe.ac.uk>
2022-03-07 12:00:04 +0000

    Removing MassRemovalIndex variable/

--- src/enzo/ActiveParticleRoutines.C
+++ src/enzo/ActiveParticleRoutines.C
@@ -264,7 +264,6 @@ void ActiveParticleType_SmartStar::SmartMerge(ActiveParticleType_SmartStar *a)
     BirthTime = a->BirthTime;
     oldmass = a->oldmass;
     TimeIndex = a->TimeIndex;
-    MassRemovalIndex = a->MassRemovalIndex; // SG. For POPIII mass removal after SetFlaggingField.
     for(int i = 0; i < NTIMES; i++)
       {
 	AccretionRateTime[i] = a->AccretionRateTime[i];
