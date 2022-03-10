321a4f214ff98f44886ef009a8f95e2713aa35a4
Simone Gordon <sgordon@cuillin.roe.ac.uk>
2022-03-10 23:08:08 +0000

    Bondi radius in CalculateBondiHoyleRadius.

--- src/enzo/Grid_CalculateSmartStarAccretionRate.C
+++ src/enzo/Grid_CalculateSmartStarAccretionRate.C
@@ -653,6 +653,6 @@ FLOAT grid::CalculateBondiHoyleRadius(float mparticle, float *vparticle, float *
   printf("%s: cInfinity = %f km/s\n", __FUNCTION__,  (cInfinity*LengthUnits/TimeUnits)/1e5);
   printf("%s: CellTemperature = %f K\n", __FUNCTION__, CellTemperature);
   printf("%s: Celllength = %e pc\n", __FUNCTION__, CellWidth[0][0]*LengthUnits/pc_cm);
-  FLOAT ret = FLOAT(2*Gcode*mparticle/(POW(cInfinity,2)));
+  FLOAT ret = FLOAT(2*Gcode*mparticle/(1 + POW(cInfinity,2)));
   return ret;
 } // SG. End of function.
