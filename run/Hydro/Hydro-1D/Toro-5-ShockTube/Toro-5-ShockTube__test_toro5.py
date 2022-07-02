1994354f5be69614d56494ddd372024a8ef2af95
John Wise <jwise@physics.gatech.edu>
2022-07-02 10:46:28 -0400

    All tests pass with yt4!

--- run/Hydro/Hydro-1D/Toro-5-ShockTube/Toro-5-ShockTube__test_toro5.py
+++ run/Hydro/Hydro-1D/Toro-5-ShockTube/Toro-5-ShockTube__test_toro5.py
@@ -10,7 +10,7 @@ _data_file = 'DD0001/data0001'
 _solution_file = 'Toro-5-ShockTube_t=0.012_exact.txt'
 _fields = [
     ('gas', 'density'),
-    ('gas', 'pressure')
+    ('gas', 'pressure'),
     ('gas', 'specific_thermal_energy')
     ]
 _les = [0.25]
