2a930056b42e6bbc3d20670684935c7c98f0fca6
clochhaas <cassilochhaas@gmail.com>
2023-12-14 14:38:03 -0500

    error fix

--- src/enzo/Grid_TestStarParticleInitializeGrid.C
+++ src/enzo/Grid_TestStarParticleInitializeGrid.C
@@ -84,7 +84,8 @@ int grid::TestStarParticleInitializeGrid(float TestStarParticleStarMass,
   }
   ParticleMass[0] = CentralMass;
   ParticleAttribute[0][0] = Time+1e-7;
-
+  ParticleAttribute[1][0] = 10.0 * Myr_s/TimeUnits;
+  
   if (STARFEED_METHOD(UNIGRID_STAR)) ParticleAttribute[1][0] = 10.0 * Myr_s/TimeUnits;
   if (STARFEED_METHOD(MOM_STAR) || STARFEED_METHOD(MECH_STAR))
     if(StarMakerExplosionDelayTime >= 0.0)
