6b72578703620c819700e3969aef95a2320bd9c3
clochhaas <cassilochhaas@gmail.com>
2024-02-27 16:51:00 -0500

    Added double particle test problem

---
+++ src/enzo/Grid_TestDoubleStarParticleInitializeGrid.C
@@ -0,0 +1,97 @@
+/***********************************************************************
+/
+/  GRID CLASS (INITIALIZE THE GRID FOR A STAR PARTICLE TEST)
+/
+/  written by: Greg Bryan
+/  date:       June, 2012
+/  modified1:
+/
+/  PURPOSE:
+/
+/  RETURNS: FAIL or SUCCESS
+/
+************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "ErrorExceptions.h"
+#include "macros_and_parameters.h"
+#include "typedefs.h"
+#include "global_data.h"
+#include "Fluxes.h"
+#include "GridList.h"
+#include "ExternalBoundary.h"
+#include "Grid.h"
+#include "phys_constants.h"
+
+int GetUnits(float *DensityUnits, float *LengthUnits,
+	     float *TemperatureUnits, float *TimeUnits,
+	     float *VelocityUnits, double *MassUnits, FLOAT Time);
+
+int grid::TestDoubleStarParticleInitializeGrid(FLOAT TestStarParticleStarMass[], 
+					 float *Initialdt,
+					 FLOAT TestStarParticleStarVelocity[][3],
+					 FLOAT TestStarParticleStarPosition[][3])
+{
+  /* declarations */
+
+  float StarMass1 = 1.0;
+  float StarMass2 = 1.0;
+  int i, dim;
+  float TestInitialdt = *Initialdt;
+  
+  /* Return if this doesn't concern us. */
+
+  if (ProcessorNumber != MyProcessorNumber)
+    return SUCCESS;
+  
+  /* Get Units. */
+
+  float TemperatureUnits = 1, DensityUnits = 1, LengthUnits = 1, 
+    VelocityUnits = 1, TimeUnits = 1;
+  double MassUnits = 1;
+  
+  if (GetUnits(&DensityUnits, &LengthUnits, &TemperatureUnits,
+	       &TimeUnits, &VelocityUnits, &MassUnits, Time) == FAIL) {
+    ENZO_FAIL("Error in GetUnits.\n");
+  }
+
+  /* Set number of particles for this grid and allocate space. */
+
+  NumberOfParticles = 2;
+  NumberOfParticleAttributes = 4;
+  this->AllocateNewParticles(NumberOfParticles);
+  printf("Allocated %d particles\n", NumberOfParticles);
+
+  /* Set particle IDs and types */
+
+  for (i = 0; i < NumberOfParticles; i++) {
+    ParticleNumber[i] = i;
+    ParticleType[i] = PARTICLE_TYPE_STAR;
+  }
+
+  /* Set central particle. */ 
+  for (i = 0; i < NumberOfParticles; i++) {
+    for (dim = 0; dim < GridRank; dim++) {
+        ParticlePosition[dim][i] = TestStarParticleStarPosition[i][dim];
+        ParticleVelocity[dim][i] = TestStarParticleStarVelocity[i][dim]*1e5*TimeUnits/LengthUnits;
+    }
+    ParticleMass[i] = TestStarParticleStarMass[i]*1.99e33* pow(LengthUnits*CellWidth[0][0],-3.0)/DensityUnits;
+    ParticleAttribute[0][i] = Time+1e-7;
+    ParticleAttribute[1][i] = 10.0 * Myr_s/TimeUnits;
+
+    if (STARFEED_METHOD(UNIGRID_STAR)) ParticleAttribute[1][i] = 10.0 * Myr_s/TimeUnits;
+    if (STARFEED_METHOD(MOM_STAR) || STARFEED_METHOD(MECH_STAR))
+        if(StarMakerExplosionDelayTime >= 0.0)
+        ParticleAttribute[1][i] = 1.0;
+        else
+        ParticleAttribute[1][i] = 10.0 * Myr_s/TimeUnits;
+    
+    ParticleAttribute[2][i] = 0.0;  // Metal fraction
+    ParticleAttribute[3][i] = 0.0;  // metalfSNIa
+  }
+
+  return SUCCESS;
+}
+
